57ce0c0c939999f9a2f69eae4a37adceed1ff077
git config --global user.name John <prashant.sharma@elogist.in>
2019-01-28 14:59:52 +0530

    admin

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
 import { Router } from '@angular/router';
+import { ApiService } from '../../../services/api2.service';
+import { CommonService } from '../../../services/common.service';
 
 @Component({
   selector: 'ngx-header',
@@ -15,14 +17,18 @@ export class HeaderComponent implements OnInit {
   @Input() position = 'normal';
 
   user: any;
-
   userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
-
+  showSuggestions = false;
+  suggestions = [];
+  foAdminName="";
+  foAdminUserId = null;
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               public router: Router,
               private userService: UserService,
-              private analyticsService: AnalyticsService) {
+              private analyticsService: AnalyticsService,
+              public api : ApiService,
+              public common : CommonService) {
   }
 
   ngOnInit() {
@@ -55,5 +61,25 @@ export class HeaderComponent implements OnInit {
       localStorage.clear();
       this.router.navigate(['/auth/login']);
     }
+  }
+
+  searchUser() {
+    this.showSuggestions = true;
+    let params = 'search=' + this.foAdminName;
+    this.api.get('Suggestion/getFoUsersList?' + params) // Customer API
+      // this.api.get3('booster_webservices/Suggestion/getElogistAdminList?' + params) // Admin API
+      .subscribe(res => {
+        console.log(res);
+        this.suggestions = res['data'];
+      }, err => {
+        console.error(err);
+        this.common.showError();
+      });
+  }
+ 
+  selectUser(user) {
+    this.foAdminName = user.name;
+    this.foAdminUserId = user.id;
+    this.showSuggestions = false;
   }
 }
