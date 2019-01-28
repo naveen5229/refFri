86e254e2fd185eca06e0e2600051e7f67e371549
Lalit Singh <lalit.singh@elogist.in>
2019-01-28 10:39:37 +0530

    animation add ts file

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
     allTags[0]['style'].display = 'none';
     console.log('All Tags: ', allTags);
     let nbCard = document.getElementsByTagName('nb-card')[0];
-    nbCard['style']['backgroundImage'] = "url('app-login-bg.jpg')";
+    nbCard['style']['backgroundImage'] = "url('../../../assets/images/app-login-bg.jpg')";
     nbCard['style']['backgroundSize'] = 'cover';
     nbCard['style']['backgroundRepeat'] = 'no-repeat';
     nbCard['style']['backgroundPosition'] = 'bottom';
