c9d16ba399bd31c5148144fc793a7869de898cc7
git config --global user.name John <prashant.sharma@elogist.in>
2019-02-15 12:49:43 +0530

    concice view update

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,8 +12,8 @@ export class ApiService {
   // URL: string = 'http://13.233.32.59/booster_webservices/'; // prod Server
   //  URL: string = 'http://13.126.215.102/booster_webservices/'; // Dev Server
   // URL: string = 'http://192.168.0.113/transtruck/booster_webservices/'; // Pawan
-  URL: string = 'http://192.168.0.109/booster_webservices/'; // Umang
-  // URL: string = 'http://localhost/transtruck/booster_webservices/';
+  //URL: string = 'http://192.168.0.109/booster_webservices/'; // Umang
+   URL: string = 'http://localhost/transtruck/booster_webservices/';
 
 
 
