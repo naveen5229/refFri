cd404741b7f33f23de8b4316b6dfcbaa93d43854
jay.kumar <jay.kumar@elogist.in>
2019-02-23 18:18:05 +0530

    Scrollbar

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
 
-  // URL: string = 'http://elogist.in/booster_webservices/'; // prod Server
- URL: string = 'http://13.126.215.102/booster_webservices/'; // Dev Server
+  URL: string = 'http://elogist.in/booster_webservices/'; // prod Server
+//  URL: string = 'http://13.126.215.102/booster_webservices/'; // Dev Server
   // URL: string = 'http://192.168.0.113/transtruck/booster_webservices/'; // Pawan
   //  URL: string = 'http://192.168.0.115/booster_webservices/'; // Umang
   // URL: string = 'http://localhost/transtruck/booster_webservices/';
