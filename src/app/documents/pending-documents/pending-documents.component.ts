2a75e9a1750ee0e195e23dd8f23b9e658cf5e0bc
Amit Kothari <amit.kothari@elogist.in>
2019-02-25 11:48:22 +0530

    updated code for doc deletion an verification

--- src/app/documents/pending-documents/pending-documents.component.ts
+++ src/app/documents/pending-documents/pending-documents.component.ts
@@ -67,5 +67,21 @@ export class PendingDocumentsComponent implements OnInit {
         console.log("response:");
         console.log(mdldata);
       });
-    } 
+    }
+    
+    deleteDocument(row) {
+      let ret = confirm("Are you sure you want to delete this Document?");
+      if(ret) {
+        console.log("Deleting document with id:" + row.id);
+        this.api.post('Vehicles/deleteDocumentById', {x_document_id: row.id})
+        .subscribe(res => {
+          this.common.loading--;
+          console.log("data", res);
+          window.location.reload();          
+        }, err => {
+          this.common.loading--;
+          console.log(err);
+        });        
+      }
+    }
 }
