5d0ca54583aae70e09a18486cc867e98c347bd59
sachinmanal <sachinelogic@gmail.com>
2019-02-28 18:33:30 +0530

    stock items and stock sub types create as telly

--- src/app/accounts/orders/orders.component.ts
+++ src/app/accounts/orders/orders.component.ts
@@ -4,6 +4,9 @@ import { CommonService } from '../../services/common.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { OrderComponent } from '../../acounts-modals/order/order.component';
 import { UserService } from '../../@core/data/users.service';
+import { DatePickerComponent } from '../../modals/date-picker/date-picker.component';
+import { TaxdetailComponent } from '../../acounts-modals/taxdetail/taxdetail.component';
+
 
 @Component({
   selector: 'orders',
@@ -12,6 +15,46 @@ import { UserService } from '../../@core/data/users.service';
 })
 export class OrdersComponent implements OnInit {
 
+  order = {
+    date: '',
+    biltynumber: '',
+    biltydate: '',
+    totalamount:0,
+    grnremarks:'',
+    branch: {
+      name: '',
+      id: ''
+    },
+    ordertype: {
+      name: '',
+      id: ''
+    },
+    ledger: {
+      name: '',
+      id: ''
+    },
+    purchaseledger: {
+      name: '',
+      id: ''
+    },
+    amountDetails: [{
+      transactionType: 'debit',
+      ledger: '',
+      taxledger: '',
+      stockitem: '',
+      stockunit: {
+      name:'',
+      id:''
+      },
+      qty: '',
+      discountledger: '',
+      warehouse: '',
+      taxDetails: '',
+      remarks:'',
+      lineamount:0
+    }]
+  };
+
   constructor(public api: ApiService,
     public common: CommonService,
     public user: UserService,
@@ -19,6 +62,7 @@ export class OrdersComponent implements OnInit {
 
   ngOnInit() {
   }
+  /*
   openModal (order?) {
     console.log('ledger123',order);
       if (order) this.common.params = order;
@@ -29,5 +73,120 @@ export class OrdersComponent implements OnInit {
         // this.addLedger(data.ledger);
         }
       });
+  } */
+
+
+  
+
+  addAmountDetails() {
+    this.order.amountDetails.push({
+      transactionType: 'debit',
+      ledger: '',
+      taxledger: '',
+      stockitem: '',
+      stockunit: {
+        name:'',
+        id:''
+        },
+      qty: '',
+      discountledger: '',
+      warehouse: '',
+      taxDetails: '',
+      remarks:'',
+      lineamount:0
+
+    });
+  }
+
+  dismiss(response) {
+    // console.log('Order:', this.order);
+    if (response) {
+      //console.log('Order new:', this.order);
+      // return;
+      this.addOrder(this.order);
     }
+   // this.activeModal.close({ response: response, Voucher: this.order });
+  }
+  getDate(date) {
+    const activeModal = this.modalService.open(DatePickerComponent, { size: 'sm', container: 'nb-layout', backdrop: 'static' });
+    activeModal.result.then(data => {
+      this.order[date] = this.common.dateFormatter(data.date).split(' ')[0];
+        console.log(this.order[date]);
+    });
+  }
+
+  TaxDetails(i) {
+    const activeModal = this.modalService.open(TaxdetailComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
+    activeModal.result.then(data => {
+      // console.log('Data: ', data);
+      if (data.response) {
+        console.log(data.taxDetails);
+        this.order.amountDetails[i].taxDetails = data.taxDetails;
+        // this.addLedger(data.ledger);
+      }
+    });
+  }
+
+  onSelected(selectedData, type, display) {
+    this.order[type].name = selectedData[display];
+    this.order[type].id = selectedData.id;
+    console.log('order User: ', this.order);
+  }
+
+  addOrder(order) {
+    console.log('new order', order);
+    // const params = {};
+    const params = {
+      billingaddress: order.billingaddress,
+      orderremarks: order.orderremarks,
+      biltynumber: order.biltynumber,
+     // code: order.code,
+      biltydatestamp: order.biltydate,
+      custcode: order.custcode,
+      orderdata: order.date,
+      deliveryterms: order.deliveryterms,
+      paymentterms: order.paymentterms,
+      vendorquotationref: order.qutationrefrence,
+      review: order.review,
+      shipmentlocation: order.shipmentlocation,
+      vendorbidref: order.vendorbidref,
+      branchid: order.branch.id,
+      ledger: order.ledger.id,
+      ordertype: order.ordertype.id,
+      purchaseledgerid: order.purchaseledger.id,
+      grnremarks: order.grnremarks,
+     // approved: order.Approved,
+     // delreview: order.delreview,
+      amountDetails: order.amountDetails
+    };
+
+    console.log('params11: ', params);
+    this.common.loading++;
+
+    this.api.post('Company/InsertPurchaseOrder', params)
+      .subscribe(res => {
+        this.common.loading--;
+        console.log('res: ', res);
+        //this.GetLedger();
+        this.common.showToast('Invoice Are Saved');
+      return;
+      }, err => {
+        this.common.loading--;
+        console.log('Error: ', err);
+        this.common.showError();
+      });
+
+
+  }
+
+ calculateTotal() {
+    let total = 0;
+    this.order.amountDetails.map(amountDetail => {
+      // console.log('Amount: ',  amountDetail.amo  unt[type]);
+        total += amountDetail.lineamount;
+    });
+    return total;
+  }
+
+
 }
