fcc636b9e980a98dacc4ee6075e9457b30569315
komal <komal.teivedi@elogist.in>
2019-03-02 12:15:52 +0530

    lorry details updated

--- src/app/admin/lorry-receipt-details/lorry-receipt-details.component.ts
+++ src/app/admin/lorry-receipt-details/lorry-receipt-details.component.ts
@@ -8,35 +8,42 @@ import { EditLorryDetailsComponent } from '../../modals/edit-lorry-details/edit-
 @Component({
   selector: 'lorry-receipt-details',
   templateUrl: './lorry-receipt-details.component.html',
-  styleUrls: ['./lorry-receipt-details.component.scss','../../pages/pages.component.css']
+  styleUrls: ['./lorry-receipt-details.component.scss', '../../pages/pages.component.css']
 })
 export class LorryReceiptDetailsComponent implements OnInit {
-   getpendinglr=[];
+  pendinglr = [];
+
   constructor(public api: ApiService, public common: CommonService,
     public user: UserService,
-    public modalService: NgbModal) { 
-     this.getPendingLr();
-    }
+    public modalService: NgbModal) {
+    this.getPendingLr();
+  }
 
   ngOnInit() {
   }
-  getPendingLr(){
-   this.common.loading++;
-   this.api.post('LorryReceiptsOperation/getPendingLr',"null")
-           .subscribe(res =>{
-             this.common.loading--;
-             console.log('res: ',res['data']);
-             this.getpendinglr=res['data'];
-           }, err =>{
-             this.common.loading--;
-             this.common.showError();
-           })
+
+  getPendingLr() {
+    this.common.loading++;
+    this.api.post('LorryReceiptsOperation/getPendingLr', {})
+      .subscribe(res => {
+        this.common.loading--;
+        console.log('res: ', res['data']);
+        this.pendinglr = res['data'];
+      }, err => {
+        this.common.loading--;
+        this.common.showError();
+      })
   }
-  openEditLorryDetailsModel(details?){
-    this.common.params={details};
+
+  openEditLorryDetailsModel(details) {
+    this.common.params = { details };
+    const activeModel = this.modalService.open(EditLorryDetailsComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' })
     this.common.handleModalSize('class', 'modal-lg', '1000');
-    const activeModel=this.modalService.open(EditLorryDetailsComponent, {size: 'lg', container: 'nb-layout', backdrop: 'static'})
-     
+    activeModel.result.then(data =>{
+      if(!data.status){
+       
+      }
+    });
   }
 
 }
--- src/app/modals/edit-lorry-details/edit-lorry-details.component.ts
+++ src/app/modals/edit-lorry-details/edit-lorry-details.component.ts
@@ -2,64 +2,73 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonService } from '../../services/common.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'edit-lorry-details',
   templateUrl: './edit-lorry-details.component.html',
   styleUrls: ['./edit-lorry-details.component.scss']
 })
 export class EditLorryDetailsComponent implements OnInit {
-  LrFlag="false";
-  InvoiceFlag="false";
-  OtherFlag="false";
-  image_url_lr="";
-  image_url_invoice="";
-  image_url_other="";
-  image_url="";
-  documents=[];
-  vehId="";
-  LrData={
-  receiptNo:"",
-  source:"",
-  dest:"",
-  remark:"",
-  taName:"",
-  consignerName:"",
-  consigneeName:"",
-  lrDate:"",
-  payType:"",
-  amount:"",
-  material:"",
-  rate:""
- };
+  images = [];
+  documents = null;
+  vehId = "";
+  LrData = {
+    receiptNo: "",
+    source: "",
+    dest: "",
+    remark: "",
+    taName: "",
+    consignerName: "",
+    consigneeName: "",
+    lrDate: "",
+    payType: "",
+    amount: "",
+    material: "",
+    rate: ""
+  };
 
   constructor(private activeModal: NgbActiveModal,
     public common: CommonService,
     public api: ApiService) {
-      if(this.common.params){
-        this.documents=this.common.params.details;
-        this.image_url_lr=this.common.params.details.lr_image;
-        console.log('image_url_lr',this.image_url_lr);
-        this.image_url_invoice=this.common.params.details.invoice_image;
-        console.log('image_url_invoice',this.image_url_invoice);
-        this.image_url_other=this.common.params.details.other;
-        console.log('image_url_other',this.image_url_other);
-      }
-     }
+    if (this.common.params) {
+      this.documents = this.common.params.details;
+      console.info('Document: ', this.documents);
+
+    }
+  }
 
   ngOnInit() {
   }
 
-  loadLr(){
-    this.image_url=this.image_url_lr;
-    console.log('image_url',this.image_url);
+  searchVehicle(vehicleList){
+    console.log('vehiclelist: '+vehicleList);
+    this.vehId=vehicleList.id;
   }
-  loadInvoice(){
-    this.image_url=this.image_url_invoice;
-    console.log('image_url',this.image_url);
+
+  loadImage(flag) {
+    if (flag == 'LR') {
+      this.images[0] = this.documents.lr_image;
+      console.log('LR', this.images[0]);
+
+    } else if (flag == 'Invoice') {
+
+      if (this.documents.invoice_image) {
+        this.images[0] = this.documents.invoice_image;
+        console.log('Invoice', this.images[0]);
+      } else { this.common.showError('Image not present!!') }
+
+    } else if (flag == 'Other') {
+
+      if (this.documents.invoice_image) {
+        this.images[0] = this.documents.other;
+        console.log('Invoice', this.images[0]);
+      } else { this.common.showError('Image not present!!') }
+    }
   }
-  loadOther(){
-    this.image_url=this.image_url_other;
-    console.log('image_url',this.image_url);
+
+  dismiss(status){
+    this.activeModal.close({status: status});
   }
+
 
 }
