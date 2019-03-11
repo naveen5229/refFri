f53ad553165bfb2f8b64aa03b6db91013d032583
Lalit Singh <lalit.singh@elogist.in>
2019-03-11 10:57:05 +0530

    change advance review option

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -19,6 +19,8 @@ import { SiteFencingComponent } from './site-fencing/site-fencing.component';
 import { DiagnosticsComponent } from './diagnostics/diagnostics.component';
 import { SiteDetailsComponent } from './site-details/site-details.component';
 import { UpdateSiteDetailsComponent } from '../modals/update-site-details/update-site-details.component';
+import { PendingDocumentsComponent } from '../documents/pending-documents/pending-documents.component'; 
+  import { from } from 'rxjs';
 
 const PAGES_COMPONENTS = [
   AdminComponent,
@@ -33,6 +35,7 @@ const PAGES_COMPONENTS = [
   SiteFencingComponent,
   DiagnosticsComponent,
   SiteDetailsComponent,
+  PendingDocumentsComponent
   
 ];
 @NgModule({
