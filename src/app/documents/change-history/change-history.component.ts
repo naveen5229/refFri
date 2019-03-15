41ef094e5cf8e562d618fe463579aa37f17545d4
Lalit <singhlalit843@gmail.com>
2019-03-15 14:05:34 +0530

    change in expiry date represention

--- src/app/documents/change-history/change-history.component.ts
+++ src/app/documents/change-history/change-history.component.ts
@@ -130,7 +130,7 @@ export class ChangeHistoryComponent implements OnInit {
         docType: { value: doc.DocumentType },
         issueDate: { value: this.datePipe.transform(doc.IssueDate, 'dd MMM yyyy') },
         wefDate: { value: this.datePipe.transform(doc.WefDate, 'dd MMM yyyy') },
-        expiryDate: { value: this.datePipe.transform(doc.ExpiryDate, 'dd MMM yyyy'), class: curr >= exp_date ? 'red' : (exp_date < nextMthDate ? 'pink' : (exp_date ? 'green' : '')) },
+        expiryDate: { value: this.datePipe.transform(doc.ExpiryDate, 'dd MMM yyyy'), class: exp_date==null && curr >= exp_date ? 'red' : (exp_date==null && exp_date < nextMthDate ? 'pink' : (doc.ExpiryDate==null ? 'default' : 'green')) },
         entryTime: { value: this.datePipe.transform(doc.EntryTime, 'dd MMM yyyy hh:mm:ss a') },
         userId: { value: doc.UserID },
         entryMode: { value: doc.EntryMode },
