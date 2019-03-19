aab949f215f476a0e083ec707755e09e9ec27d7c
Amit Kothari <amit.kothari@elogist.in>
2019-03-19 13:37:37 +0530

    fixed issue in change history pg

--- src/app/documents/change-history/change-history.component.ts
+++ src/app/documents/change-history/change-history.component.ts
@@ -127,9 +127,10 @@ export class ChangeHistoryComponent implements OnInit {
    
     console.log("Data=",this.data);
     this.data.map(doc => {
+      this.valobj = {};
       for(let i = 0; i < this.headings.length; i++) {
         console.log("doc index value:",doc[this.headings[i]]);
-        this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action : ''};
+        this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action : ''};        
       }
 
       // let exp_date = this.common.dateFormatter(doc.expiry_date).split(' ')[0];
