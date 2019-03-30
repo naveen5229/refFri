222cf51915bcad34005ea732dcb9cc34fc7d283f
Lalit <singhlalit843@gmail.com>
2019-03-30 14:53:31 +0530

    change in document

---
+++ src/app/driver/driver-document/driver-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'driver-document',
+  templateUrl: './driver-document.component.html',
+  styleUrls: ['./driver-document.component.scss']
+})
+export class DriverDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
