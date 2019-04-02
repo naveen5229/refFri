0667bb24af0bfe38edf4a94a90f25c9613ec703f
Lalit <singhlalit843@gmail.com>
2019-04-02 18:43:22 +0530

    change in vehicle anaysis

--- src/app/modals/vehicle-analysis/vehicle-analysis.component.ts
+++ src/app/modals/vehicle-analysis/vehicle-analysis.component.ts
@@ -5,22 +5,45 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'vehicle-analysis',
   templateUrl: './vehicle-analysis.component.html',
-  styleUrls: ['./vehicle-analysis.component.scss','../../pages/pages.component.css']
+  styleUrls: ['./vehicle-analysis.component.scss', '../../pages/pages.component.css']
 })
 export class VehicleAnalysisComponent implements OnInit {
-data = [];
+  data = [];
+  columns = [];
   constructor(public mapService: MapService,
     public common: CommonService,
-    private activeModal: NgbActiveModal
-  ) {
-    this.data=this.common.params;
-   }
+    private activeModal: NgbActiveModal) {
+    this.data = this.common.params;
+    this.common.handleModalSize('class', 'modal-lg', '1000');
+    this.getdata();
+  }
 
   ngOnInit() {
   }
-  
+
   closeModal() {
     this.activeModal.close();
   }
 
+  getdata() {
+    // this.data = this.data.length;
+    if (this.data.length) {
+      for (var key in this.data[0]) {
+        if (key.charAt(0) != "_")
+          this.columns.push(key);
+      }
+      console.log("columns");
+      console.log(this.columns);
+    }
+
+  }
+
+  formatTitle(strval) {
+    let pos = strval.indexOf('_');
+    if (pos > 0) {
+      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+    } else {
+      return strval.charAt(0).toUpperCase() + strval.substr(1);
+    }
+  }
 }
