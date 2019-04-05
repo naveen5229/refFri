eb6fdcdaa845185a9ed113dc1d3626c404329f8d
Lalit <singhlalit843@gmail.com>
2019-04-05 13:22:28 +0530

    change in account page

--- src/app/accounts/account/account.component.ts
+++ src/app/accounts/account/account.component.ts
@@ -10,14 +10,14 @@ import { UserService } from '../../@core/data/users.service';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
-Accounts =[];
+  Accounts = [];
   constructor(public api: ApiService,
     public common: CommonService,
     public user: UserService,
-    public modalService: NgbModal) { 
+    public modalService: NgbModal) {
     this.GetAccount();
     this.common.currentPage = 'Account';
-    }
+  }
 
   ngOnInit() {
   }
@@ -25,7 +25,7 @@ Accounts =[];
     let params = {
       foid: 123
     };
-    
+
     this.common.loading++;
     this.api.post('Accounts/GetAccount', params)
       .subscribe(res => {
@@ -41,42 +41,75 @@ Accounts =[];
 
   }
 
-  openAccountModal (Accounts?) {
-   // console.log('Accounts',Accounts);
-      if (Accounts) this.common.params = Accounts;
-      const activeModal = this.modalService.open(AccountsComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static',keyboard :false,windowClass : "accountModalClass" });
+  openAccountModal(Accounts?) {
+
+    if (Accounts) {
+      console.log('Accounts', Accounts);
+      this.common.params = Accounts;
+      const activeModal = this.modalService.open(AccountsComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static', keyboard: false, windowClass: "accountModalClass" });
       activeModal.result.then(data => {
-        // console.log('Data: ', data);
         if (data.response) {
-         this.addAccount(data.Accounts);
+          this.updateAccount(data.Accounts,Accounts.id);
+          return
         }
       });
     }
-
-    addAccount(Accounts) {
-      console.log('accountdata',Accounts);
-     // const params ='';
-      const params = {
-          name: Accounts.name,
-          foid: Accounts.user.id,
-          parentid: Accounts.account.id,
-          primarygroupid: Accounts.account.primarygroup_id,
-          x_id:0
-       };
-  
-       console.log('params11: ',params);
-      this.common.loading++;
-  
-      this.api.post('Accounts/InsertAccount', params)
-        .subscribe(res => {
-          this.common.loading--;
-          console.log('res: ', res);
-          this.GetAccount();
-        }, err => {
-          this.common.loading--;
-          console.log('Error: ', err);
-          this.common.showError();
-        });
-  
+    else {
+      this.common.params = null;
+      const activeModal = this.modalService.open(AccountsComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static', keyboard: false, windowClass: "accountModalClass" });
+      activeModal.result.then(data => {
+        if (data.response) {
+          this.addAccount(data.Accounts);
+          return;
+        }
+      });
     }
+  }
+
+  addAccount(Accounts) {
+    console.log('accountdata', Accounts);
+    const params = {
+      name: Accounts.name,
+      foid: Accounts.user.id,
+      parentid: Accounts.account.id,
+      primarygroupid: Accounts.account.primarygroup_id,
+      x_id: 0
+    };
+    console.log('params11: ', params);
+    this.common.loading++;
+    this.api.post('Accounts/InsertAccount', params)
+      .subscribe(res => {
+        this.common.loading--;
+        console.log('res: ', res);
+        this.GetAccount();
+      }, err => {
+        this.common.loading--;
+        console.log('Error: ', err);
+        this.common.showError();
+      });
+  }
+  updateAccount(Accounts,rowid){
+    console.log('updated data', Accounts);
+    const params = {
+      name: Accounts.name,
+      foid:123,
+      parentid: Accounts.account.id,
+      primarygroupid: Accounts.account.primarygroup_id,
+      x_id: rowid
+    };
+    console.log('params11: ', params);
+    this.common.loading++;
+    this.api.post('Accounts/InsertAccount', params)
+      .subscribe(res => {
+        this.common.loading--;
+        console.log('res: ', res);
+        this.GetAccount();
+      }, err => {
+        this.common.loading--;
+        console.log('Error: ', err);
+        this.common.showError();
+      });
+
+  }
+
 }
