5b20883b10254319853d13e8d4b25f982adc4796
sachinmanal <sachinelogic@gmail.com>
2019-04-10 19:10:21 +0530

    ledger colum add

--- src/app/accounts/ledgers/ledgers.component.ts
+++ src/app/accounts/ledgers/ledgers.component.ts
@@ -102,6 +102,10 @@ export class LedgersComponent implements OnInit {
       primarygroupid: ledger.undergroup.primarygroup_id,
       account_id: ledger.undergroup.id,
       accDetails: ledger.accDetails,
+      branchname :ledger.branchname,
+      branchcode:  ledger.branchcode,
+      accnumber:   ledger.accnumber,
+      creditdays:  ledger.creditdays,
       x_id: ledger.id ? ledger.id : 0,
     };
 
