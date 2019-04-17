da3225add04a20b230662393234262e5dafb2b54
Lalit <singhlalit843@gmail.com>
2019-04-17 16:46:52 +0530

    change in alert issue update

--- src/app/admin/alert-related-issue/alert-related-issue.component.ts
+++ src/app/admin/alert-related-issue/alert-related-issue.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ApiService } from '../../services/api.service';
 import { CommonService } from '../../services/common.service';
-
 import { ViewListComponent } from '../../modals/view-list/view-list.component';
 import { ResolveMissingIndustryComponent } from '../../modals/resolve-missing-industry/resolve-missing-industry.component';
 import { ChangeVehicleStatusComponent } from '../../modals/change-vehicle-status/change-vehicle-status.component';
@@ -20,13 +18,7 @@ export class AlertRelatedIssueComponent implements OnInit {
   missing = 0;
   backlog = 0;
   tickets = 1;
-  // backlogData = [];
-  // columns = [];
-  // header = [];
-  // header2 = [];
-  // columns2 = [];
-  // distance = 1;
-  // ratio = 3;
+  table = null;
 
 
   constructor(public api: ApiService,
@@ -50,6 +42,7 @@ export class AlertRelatedIssueComponent implements OnInit {
         this.common.loading--;
         console.log(res);
         this.ticketsData = res['data'];
+        this.table = this.setTable();
         console.log("data:", this.ticketsData);
         if (this.ticketsData) {
           this.tickets = 1;
@@ -62,6 +55,61 @@ export class AlertRelatedIssueComponent implements OnInit {
       });
 
   }
+
+  setTable() {
+    let headings = {
+      vehicleNumber: { title: 'Vehicle Number', placeholder: 'Vehicle No' },
+      regno: { title: 'Regno ', placeholder: 'Regno '  },
+      ltime: { title: 'Ltime', placeholder: 'Ltime' },
+      ttime: { title: 'Ttime', placeholder: 'Ttime' },
+      disa: { title: 'Disa', placeholder: 'Disa' },
+      dis: { title: 'Dis', placeholder: 'Dis' },
+      ratio: { title: 'Ratio', placeholder: 'Ratio' },
+      foname: { title: 'Fo Name', placeholder: 'Fo Name' },
+      holdBy: { title: 'Hold By', placeholder: 'Hold By' },
+      remark: { title: 'Remark', placeholder: 'Remark' },
+      action: { title: 'Action', placeholder: 'Action', hideSearch: true, class: 'del' },
+    };
+
+    
+    return {
+      data: {
+        headings: headings,
+        columns: this.getTableColumns()
+      },
+      settings: {
+        hideHeader: true,
+        tableHeight: "auto"
+      }
+    }
+  }
+
+  getTableColumns() {
+    let columns = [];
+    this.ticketsData.map(ticket => {
+
+      let column = {
+        vehicleNumber: { value: ticket.vehicle_id },
+        regno: { value: ticket.regno },
+        ltime: { value: ticket.ltime ,date:'dd MMM HH:mm' },
+        ttime: { value: ticket.ttime , date:'dd MMM HH:mm' },
+        disa: { value: ticket.disa },
+        dis: { value: ticket.dis },
+        ratio: { value: ticket.ratio },
+        foname: { value: ticket.fo_name },
+        holdBy:{value : ticket.hold_by},
+        remark: { value: ticket.remark },
+        action: { value: `<i class="fa fa-pencil-alt"></i>`, isHTML: true, action: this.enterTicket.bind(this, ticket), class: 'icon text-center del' },
+      };
+     
+
+      columns.push(column);
+    });
+    return columns;
+  }
+
+
+
   enterTicket(issue) {
     let result;
     let params = {
@@ -147,30 +195,6 @@ export class AlertRelatedIssueComponent implements OnInit {
 
   }
 
-  // backlogsIssue(backlogsIssue) {
-  //   let ltime = new Date(backlogsIssue.addtime);
-  //   let subtractLTime = new Date(ltime.setHours(ltime.getHours() - 48));
-  //   let latch_time = this.common.dateFormatter(subtractLTime);
-  //   console.log("issue:", backlogsIssue);
-  //   let VehicleStatusData = {
-  //     vehicle_id: backlogsIssue.x_vehicle_id,
-  //     ttime: backlogsIssue.y_sec_start_time,
-  //     suggest: null,
-  //     latch_time: backlogsIssue.y_start_time,
-  //     status: 3
-  //   }
-  //   console.log("VehicleStatusData", VehicleStatusData);
-
-  //   this.common.params = VehicleStatusData;
-  //   const activeModal = this.modalService.open(ResolveMissingIndustryComponent, { size: 'lg', container: 'nb-layout' });
-  //   activeModal.result.then(data => {
-  //   });
-
-  // }
-
-
-  // formatTitle(title) {
-  //   return title.charAt(0).toUpperCase() + title.slice(1);
-  // }
+ 
 
 }
