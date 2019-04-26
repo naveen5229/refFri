1929852d1cdabf2ab02c540155b1fe6b37e5b5bf
Prashant <prashant.sharma@elogist.in>
2019-04-26 15:50:22 +0530

    change driver

--- src/app/pages/placement-delay-faults/placement-delay-faults.component.ts
+++ src/app/pages/placement-delay-faults/placement-delay-faults.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonService } from '../../services/common.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { VehicleTripUpdateComponent } from '../../modals/vehicle-trip-update/vehicle-trip-update.component';
 
 @Component({
   selector: 'placement-delay-faults',
@@ -10,9 +11,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class PlacementDelayFaultsComponent implements OnInit {
 
-  showTable=false;
+  showTable = false;
 
-  delayFaults=[];
+  delayFaults = [];
   headings = [];
   valobj = {};
   table = {
@@ -27,14 +28,14 @@ export class PlacementDelayFaultsComponent implements OnInit {
 
   constructor(public api: ApiService,
     public common: CommonService,
-   public modalService: NgbModal) { 
-     this.getDelayFaults();
-   }
+    public modalService: NgbModal) {
+    this.getDelayFaults();
+  }
 
   ngOnInit() {
   }
 
-  getDelayFaults(){
+  getDelayFaults() {
 
     this.delayFaults = [];
     this.table = {
@@ -51,32 +52,30 @@ export class PlacementDelayFaultsComponent implements OnInit {
     this.api.get('Placement/placementDelayFaults?')
       .subscribe(res => {
         this.common.loading--;
-        console.log('res: ', res['data'])
-        this.delayFaults =JSON.parse(res['data'][0].fn_placements_getfaults);
-        if(this.delayFaults!=null)
-        {
-        console.log('delayFaults',this.delayFaults);
-        let first_rec = this.delayFaults[0];
-        console.log("first_Rec", first_rec);
-        
-        for (var key in first_rec) {
-          if(key.charAt(0) != "_") {
-            this.headings.push(key);
-            let headerObj = { title: key, placeholder: this.formatTitle(key) };
-            this.table.data.headings[key] = headerObj;
+        this.delayFaults = JSON.parse(res['data'][0].fn_placements_getfaults);
+        if (this.delayFaults != null) {
+          console.log('delayFaults', this.delayFaults);
+          let first_rec = this.delayFaults[0];
+          console.log("first_Rec", first_rec);
+
+          for (var key in first_rec) {
+            if (key.charAt(0) != "_") {
+              this.headings.push(key);
+              let headerObj = { title: key, placeholder: this.formatTitle(key) };
+              this.table.data.headings[key] = headerObj;
+            }
+
           }
 
+          this.table.data.columns = this.getTableColumns();
+          console.log("table:");
+          console.log(this.table);
+          this.showTable = true;
+        } else {
+          this.common.showToast('No Record Found !!');
         }
 
-        this.table.data.columns = this.getTableColumns();
-        console.log("table:");
-        console.log(this.table);
-        this.showTable=true;
-      }else{
-        this.common.showToast('No Record Found !!');
-      }
 
-              
       }, err => {
         this.common.loading--;
         this.common.showError();
@@ -86,23 +85,46 @@ export class PlacementDelayFaultsComponent implements OnInit {
 
   getTableColumns() {
     let columns = [];
-    for(var i= 0; i<this.delayFaults.length; i++) {
+    for (var i = 0; i < this.delayFaults.length; i++) {
       this.valobj = {};
-      for(let j=0; j<this.headings.length; j++) {j 
-          this.valobj[this.headings[j]] = {value: this.delayFaults[i][this.headings[j]], class: 'black', action:  ''};
+      for (let j = 0; j < this.headings.length; j++) {
+        if (this.headings[j] == "startpt") {
+          console.log("headings[j]", this.headings[j]);
+          this.valobj[this.headings[j]] = { value: this.delayFaults[i][this.headings[j]], class: 'black', action: this.openPlacementModal.bind(this, this.delayFaults[i]) };
+        } else {
+          this.valobj[this.headings[j]] = { value: this.delayFaults[i][this.headings[j]], class: 'black', action: '' };
+        }
+
       }
+      this.valobj['style'] = { background: this.delayFaults[i]._rowcolor };
       columns.push(this.valobj);
     }
+
+    console.log('Columns:', columns);
     return columns;
   }
 
   formatTitle(strval) {
     let pos = strval.indexOf('_');
-    if(pos > 0) {
-      return strval.toLowerCase().split('_').map(x=>x[0].toUpperCase()+x.slice(1)).join(' ')
+    if (pos > 0) {
+      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
     } else {
       return strval.charAt(0).toUpperCase() + strval.substr(1);
     }
   }
 
+  openPlacementModal(placement) {
+    console.log("openPlacementModal", placement);
+    let tripDetails = {
+      vehicleId: placement._vid,
+      siteId: placement._site_id
+    }
+    this.common.params = { tripDetils: tripDetails, ref_page: 'placements' };
+    console.log("vehicleTrip", tripDetails);
+    const activeModal = this.modalService.open(VehicleTripUpdateComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
+    activeModal.result.then(data => {
+      //console.log("data", data.respone);
+
+    });
+  }
 }
