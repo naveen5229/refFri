2991d2f8292af76a3f72ca601e86d885fdf3c6b9
Lalit <singhlalit843@gmail.com>
2019-05-03 15:02:54 +0530

    change in trip details

--- src/app/modals/trip-details/trip-details.component.ts
+++ src/app/modals/trip-details/trip-details.component.ts
@@ -4,72 +4,74 @@ import { ApiService } from '../../services/api.service';
 import { VehicleReportComponent } from '../vehicle-report/vehicle-report.component';
 import { RouteMapperComponent } from '../route-mapper/route-mapper.component';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DateService } from '../../services/date.service';
 
 @Component({
   selector: 'trip-details',
   templateUrl: './trip-details.component.html',
-  styleUrls: ['./trip-details.component.scss','../../pages/pages.component.css']
+  styleUrls: ['./trip-details.component.scss', '../../pages/pages.component.css']
 })
 export class TripDetailsComponent implements OnInit {
-  startDate=null;
-  endDate=null;
+  startDate = null;
+  endDate = null;
   vehicleId = null;
   vehicleRegNo = null;
   trips = null;
   constructor(
-    public common : CommonService,
-    public api : ApiService,
+    public common: CommonService,
+    public api: ApiService,
     private modalService: NgbModal,
     public activeModal: NgbActiveModal,
-  ) { 
-   // this.common.handleModalSize('class', 'modal-lg', '1600');
+    public dateService: DateService
+  ) {
+    // this.common.handleModalSize('class', 'modal-lg', '1600');
     this.startDate = this.common.params.fromTime;
-  this.endDate = this.common.params.toTime;
-  this.vehicleId = this.common.params.vehicleId;
-  this.vehicleRegNo = this.common.params.vehicleRegNo;
-  this.getTripDetails();
+    this.endDate = this.common.params.toTime;
+    this.vehicleId = this.common.params.vehicleId;
+    this.vehicleRegNo = this.common.params.vehicleRegNo;
+    this.getTripDetails();
   }
 
   ngOnInit() {
   }
 
-  getTripDetails(){
-    let params = "vehicleId=" +this.vehicleId+
-    "&startTime=" +this.common.dateFormatter(this.startDate)+
-    "&endTime=" +this.common.dateFormatter(this.endDate);
+  getTripDetails() {
+    let params = "vehicleId=" + this.vehicleId +
+      "&startTime=" + this.common.dateFormatter(this.startDate) +
+      "&endTime=" + this.common.dateFormatter(this.endDate);
     console.log(params)
-    this.api.get('TripsOperation/viewBtw?' +params)
-        .subscribe(res => {
-          console.log('Res: ', res['data']);
-          this.trips = res['data'];
-        }, err => {
-          console.error(err);
-          this.common.showError();
-        });
+    this.api.get('TripsOperation/viewBtw?' + params)
+      .subscribe(res => {
+        console.log('Res: ', res['data']);
+        this.trips = res['data'];
+      }, err => {
+        console.error(err);
+        this.common.showError();
+      });
   }
   vehicleReport(trip) {
-    console.log("trip------",trip);
-    let fromTime =trip.start_time;
-    let toTime= trip.end_time;
-    console.log("trip------",fromTime,toTime);
-    this.common.params = {vehicleId:trip.vehicle_id,vehicleRegNo:this.vehicleRegNo,fromTime:fromTime,toTime:toTime};
-    this.common.handleModalHeightWidth('class', 'modal-lg', '200','1500');   
-    this.modalService.open(VehicleReportComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static',windowClass: "mycustomModalClass" });
+    console.log("trip------", trip);
+    let fromTime = trip.start_time;
+    let toTime = trip.end_time;
+    console.log("trip------", fromTime, toTime);
+    this.common.params = { vehicleId: trip.vehicle_id, vehicleRegNo: this.vehicleRegNo, fromTime: fromTime, toTime: toTime };
+    this.common.handleModalHeightWidth('class', 'modal-lg', '200', '1500');
+    this.modalService.open(VehicleReportComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static', windowClass: "mycustomModalClass" });
 
   }
 
-  openRouteMapper(trip){
-    let fromTime =this.common.dateFormatter(new Date(trip.start_time));
-    let toTime= this.common.dateFormatter(new Date(trip.end_time));
-    this.common.params = {vehicleId:trip.vehicle_id,vehicleRegNo:this.vehicleRegNo,fromTime:fromTime,toTime:toTime}
+  openRouteMapper(trip) {
+    let fromTime = this.common.dateFormatter(new Date(trip.start_time));
+    let toTime = this.common.dateFormatter(new Date(trip.end_time));
+    this.common.params = { vehicleId: trip.vehicle_id, vehicleRegNo: this.vehicleRegNo, fromTime: fromTime, toTime: toTime }
     // console.log("open Route Mapper modal", this.common.params);
-    const activeModal = this.modalService.open(RouteMapperComponent, { size: 'lg', container: 'nb-layout',windowClass: "mycustomModalClass" });
+    const activeModal = this.modalService.open(RouteMapperComponent, { size: 'lg', container: 'nb-layout', windowClass: "mycustomModalClass" });
     activeModal.result.then(data =>
-      console.log("data",data) 
+      console.log("data", data)
       // this.reloadData()
-      );
-    }
-    
+    );
+  }
+
   closeModal() {
     this.activeModal.close();
   }
