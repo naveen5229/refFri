60b8876c92f736e169c744cf814b6e8885d0db48
goutam <goutam@elogist.in>
2019-05-07 16:02:43 +0530

    policeStation

--- src/app/modals/police-station/police-station.component.ts
+++ src/app/modals/police-station/police-station.component.ts
@@ -3,11 +3,13 @@ import { MapService } from '../../services/map.service';
 import { ApiService } from '../../services/api.service';
 import { CommonService } from '../../services/common.service';
 import { Api2Service } from '../../services/api2.service';
+import { element } from '@angular/core/src/render3';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'police-station',
   templateUrl: './police-station.component.html',
-  styleUrls: ['./police-station.component.scss']
+  styleUrls: ['./police-station.component.scss','../../pages/pages.component.css']
 })
 export class PoliceStationComponent implements OnInit {
   PoliceStation = [];
@@ -19,6 +21,7 @@ export class PoliceStationComponent implements OnInit {
   constructor(
     public mapService: MapService,
     public api2: Api2Service,
+    private activeModal: NgbActiveModal,
     public common: CommonService) { 
       this.common.handleModalSize('class', 'modal-lg', '1000');
       this.lat = this.common.params.lat;
@@ -34,7 +37,7 @@ export class PoliceStationComponent implements OnInit {
     this.mapService.mapIntialize("map");
     
   }
-
+  tempData = [];
   getPoliceStation(){
     
     let params = "lat="+this.lat+
@@ -47,15 +50,38 @@ export class PoliceStationComponent implements OnInit {
         .subscribe(res => {
           this.common.loading--;
           console.log(res['data'])
+          
 
           if (res['success'])
             this.common.showToast('Success');
             this.PoliceStation = res['data'];
-            this.mapService.createMarkers(this.PoliceStation);
+            this.mapService.createMarkers(this.PoliceStation,false,true,["vicinity"]);
+            
         }, err => {
           this.common.loading--;
           this.common.showError();
         })
+        
+        
+        
     
+  }
+  openSmartTool(i, value) {
+    this.PoliceStation.forEach(vEvent => {
+      if (vEvent != value)
+        vEvent.isOpen = false;
+    });
+    value.isOpen = !value.isOpen;
+    this.zoomFunctionality(i, value);
+  }
+  zoomFunctionality(i, value) {
+    console.log("value", value);
+    let latLng = this.mapService.getLatLngValue(value);
+    let googleLatLng = this.mapService.createLatLng(latLng.lat, latLng.lng);
+    console.log("latlngggg", googleLatLng);
+    this.mapService.zoomAt(googleLatLng);
+  }
+  dismiss() {
+    this.activeModal.close();
   }
 }
