1964229f508488931bb99b611faa9b23cb23a108
Lalit <singhlalit843@gmail.com>
2019-05-08 12:15:53 +0530

    change in doc

---
+++ src/app/vehicle-maintenance/vehicle-maintenance.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { VehicleMaintenanceComponent } from './vehicle-maintenance-components';
+import { VehicleMaintenanceRoutingModule } from './vehicle-maintenance-routing.module';
+import { ThemeModule } from '../@theme/theme.module';
+import { DashboardModule } from '../documents/dashboard/dashboard.module';
+import { DirectiveModule } from '../directives/directives.module';
+import { ImageViewerModule } from 'ng2-image-viewer';
+import { AddVehicleMaintenanceComponent } from './add-vehicle-maintenance/add-vehicle-maintenance.component';
+
+const PAGES_COMPONENTS = [
+  VehicleMaintenanceComponent,
+];
+@NgModule({
+
+  imports: [
+    VehicleMaintenanceRoutingModule,
+    ThemeModule,
+    DashboardModule,
+    DirectiveModule,
+    ImageViewerModule
+  ],
+  providers: [DatePipe],
+  declarations: [
+    ...PAGES_COMPONENTS,
+    AddVehicleMaintenanceComponent,
+
+  ],
+})
+export class VehicleMaintenanceModule { }
