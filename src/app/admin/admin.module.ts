66d3248b5a7212678c253b4d8663ede86b6106ca
Your Name <komal.trivedi@elogist.in>
2019-06-07 11:02:35 +0530

    mapped-fuel-voucher

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -53,7 +53,6 @@ import { PendingVehicleComponent } from './pending-vehicle/pending-vehicle.compo
 import { HaltDensityComponent } from './halt-density/halt-density.component';
 import { PalacementSiteRuleComponent } from './placement-site-rule/palacement-site-rule.component';
 import { FuelAverageIssuesComponent } from './fuel-average-issues/fuel-average-issues.component';
-import { ConsolidateFuelAverageComponent } from './consolidate-fuel-average/consolidate-fuel-average.component';
 import { FuelAnalysisComponent } from './fuel-analysis/fuel-analysis.component';
 import { TripAnalysisComponent } from './trip-analysis/trip-analysis.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
@@ -83,7 +82,7 @@ const PAGES_COMPONENTS = [
   FuelRulesComponent,
   PumpStationAreaComponent,
   TollTransactionSummaryComponent,
-  ConsolidateFuelAverageComponent,
+  //  ConsolidateFuelAverageComponent,
   FuelAverageIssuesComponent,
   FuelAnalysisComponent,
   TripAnalysisComponent
@@ -132,7 +131,7 @@ const PAGES_COMPONENTS = [
     PendingVehicleComponent,
     HaltDensityComponent,
     PalacementSiteRuleComponent,
-    ConsolidateFuelAverageComponent,
+    //  ConsolidateFuelAverageComponent,
     FuelAverageIssuesComponent,
     FuelAnalysisComponent,
     TripAnalysisComponent,
