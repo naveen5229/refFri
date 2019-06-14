49dc508ae7b5d9d193b1b77e56f6f749dfb677e4
Hrithik <hrithik.sharma@elogist.in>
2019-06-14 21:41:09 +0530

    attendance

--- src/app/driver/driver-attendance/driver-attendance.component.ts
+++ src/app/driver/driver-attendance/driver-attendance.component.ts
@@ -6,6 +6,7 @@ import { ApiService } from '../../services/api.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DriverAttendanceUpdateComponent } from '../../modals/driver-attendance-update/driver-attendance-update.component';
 import * as _ from "lodash";
+import { PARAMETERS } from '@angular/core/src/util/decorators';
 
 @Component({
   selector: 'driver-attendance',
@@ -30,11 +31,17 @@ export class DriverAttendanceComponent implements OnInit {
     data: {
       headings: {
         name: { placeholder: 'Driver' },
-        total_present: { placeholder: 'TP' },
-        total_absent: { placeholder: 'TA' },
-        total_half: { placeholder: 'THD' }
+        total_present: { placeholder: 'TP', editable: true },
+        total_absent: { placeholder: 'TA', editable: true },
+        total_half: { placeholder: 'THD', editable: true },
+        isEdit: { placeholder: 'isEdit', editable: true }
+
       },
       columns: []
+
+
+
+
     },
     settings: {
       //hideHeader: true
@@ -43,6 +50,7 @@ export class DriverAttendanceComponent implements OnInit {
       editableAction: this.handleTableEdit.bind(this)
     }
   };
+  user: any;
 
   //dateValue = [];
 
@@ -91,7 +99,7 @@ export class DriverAttendanceComponent implements OnInit {
         this.common.loading--;
         console.log('res', res['data']);
         this.attendances = res['data'];
-        this.table.data.columns = this.table.data.columns = this.getTableColumns(this.formattData());
+        this.table.data.columns = this.getTableColumns(this.formattData());
       }, err => {
         this.common.loading--;
         console.log(err);
@@ -110,16 +118,21 @@ export class DriverAttendanceComponent implements OnInit {
         total_present: driverAttendanceGroups[key][0].total_present,
         total_absent: driverAttendanceGroups[key][0].total_absent,
         total_half: driverAttendanceGroups[key][0].total_half,
-
+        id: driverAttendanceGroups[key][0].attendance_id,
+        driver_id: driverAttendanceGroups[key].driver_id,
+        date: driverAttendanceGroups[key][0].date,
         data: [...driverAttendanceGroups[key]]
       })
     });
     formattedAttendances[0]['data'].map(attendance => {
-      this.table.data.headings[attendance.date.split(' ')[0]] = { placeholder: this.common.changeDateformat3(attendance.date.split(' ')[0]) };
+      this.table.data.headings[attendance.date.split(' ')[0]] = {
+        placeholder: this.common.changeDateformat3(attendance.date.split(' ')[0]),
+        editable: true
+      };
       //console.log('dates', this.dateValue);
     });
 
-    //console.log('Group:', this.dateValue);
+    // console.log('Group:', formattedAttendances);
     return formattedAttendances;
   }
 
@@ -133,24 +146,68 @@ export class DriverAttendanceComponent implements OnInit {
         total_present: { value: formattedAttendance.total_present },
         total_absent: { value: formattedAttendance.total_absent },
         total_half: { value: formattedAttendance.total_half },
-
-
-
-
+        isEdit: { value: '' },
+        id: formattedAttendance.id
       };
       formattedAttendance.data.map(attendance => {
-        column[attendance.date.split(' ')[0]] = { value: attendance.attendance }
+        column[attendance.date.split(' ')[0]] = { value: attendance.attendance, date: attendance.date }
       });
+      // column['params'] = formattedAttendance;
       columns.push(column);
     });
     return columns;
   }
-  view(formattedAttendance) {
-    this.common.params = { formattedAttendance }
-    console.log('view', this.common.params);
-  }
-  handleTableEdit() {
 
+
+  handleTableEdit(columns, formattedAttendance) {
+    let att_list=[];
+  
+    this.common.params = { columns };
+    // console.log('current', this.common.params.columns.current);
+    let show = [];
+    console.log('columns', columns);
+    let currentVal=this.common.params.columns.current;
+    for(let i=0;i<currentVal.length;i++){
+
+      att_list.push({
+        "driver_name":currentVal[i]["name"],
+        "tp":currentVal[i]["name"]
+      })
+
+      
+      
+    }
+    // Object.keys(this.common.params.columns.current).forEach(date => {
+    //   show.push(this.common.params.columns.current[date]);
+    // });
+    console.log("Show", show);
+
+    //let att_list = _.groupBy(this.common.params.columns.current, '');
+
+    // let att_list = [{
+    //   //foid: this.user._details.id,
+    //   driver_id: this.common.params.columns.current.id,
+    //   driver_name: this.common.params.columns.current.name,
+    //   date: this.common.params.columns.current.date,
+    //   tp: this.common.params.columns.current.total_present,
+    //   ta: this.common.params.columns.current.total_absent,
+    //   thd: this.common.params.columns.current.total_half,
+    //   // adjust_day: this.common.params.current.isEdit,
+    //   att_id: this.common.params.columns.current.id,
+
+
+    // }]
+    // this.common.loading++;
+    // this.api.post('Drivers/updateDriverAttendance')
+    //   .subscribe(res => {
+    //     this.common.loading--;
+    //     console.log('res', res['data']);
+
+
+    //   }, err => {
+    //     this.common.loading--;
+    //     console.log(err);
+    //   });
   }
 }
 
