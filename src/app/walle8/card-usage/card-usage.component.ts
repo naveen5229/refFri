d9a8434f6449aaf50be67e4f86dcb25fdc78c417
Lalit <singhlalit843@gmail.com>
2019-06-19 11:47:00 +0530

    change in pdf file

--- src/app/walle8/card-usage/card-usage.component.ts
+++ src/app/walle8/card-usage/card-usage.component.ts
@@ -105,7 +105,7 @@ export class CardUsageComponent implements OnInit {
         let fodata = res['data'];
         let left_heading = fodata['name'];
         let center_heading = "Card Usage";
-        this.common.getPDFFromTableId(tblEltId, left_heading, center_heading);
+        this.common.getPDFFromTableId(tblEltId, left_heading, center_heading, null, '');
       }, err => {
         this.common.loading--;
         console.log(err);
