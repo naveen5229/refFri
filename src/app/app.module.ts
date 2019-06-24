b0e7f5a3a04fb860eab1aa5957ff40f9dd7f8796
Prashant <prashant.sharma@elogist.in>
2019-06-24 10:08:22 +0530

    frieght rate input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -172,6 +172,8 @@ import { FuelEditComponent } from './modals/modal-wise-fuel-avg/fuel-edit/fuel-e
 import { AddViaRoutesComponent } from './modals/add-via-routes/add-via-routes.component';
 import { VehicleTyreSummaryComponent } from './modals/Tyres/vehicle-tyre-summary/vehicle-tyre-summary.component';
 import { LoadHaltComponent } from './modals/load-halts/load-halt.component';
+import { FreightInputWithoutLocationComponent } from './modals/FreightRate/freight-input-without-location/freight-input-without-location.component';
+import { FreightInputLocationComponent } from './modals/FreightRate/freight-input-location/freight-input-location.component';
 
 
 
@@ -318,11 +320,15 @@ import { LoadHaltComponent } from './modals/load-halts/load-halt.component';
     FuelEditComponent,
     AddViaRoutesComponent,
     VehicleTyreSummaryComponent,
+    FreightInputWithoutLocationComponent,
+    FreightInputLocationComponent,
 
 
 
   ],
   entryComponents: [
+    FreightInputWithoutLocationComponent,
+    FreightInputLocationComponent,
     VehicleTyreSummaryComponent,
     BulkVehicleNextServiceDetailComponent,
     AddAdvancedMaintenanceComponent,
