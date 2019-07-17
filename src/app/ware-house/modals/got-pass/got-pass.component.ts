9098fef095144a4e91e39b3bfa14a234ea384f8f
Amit Kothari <amit.kothari@elogist.in>
2019-07-17 15:46:09 +0530

    get pass

--- src/app/ware-house/modals/got-pass/got-pass.component.ts
+++ src/app/ware-house/modals/got-pass/got-pass.component.ts
@@ -11,12 +11,18 @@ import { ApiService } from '../../../services/api.service';
 })
 export class GotPassComponent implements OnInit {
   itemList = [];
+  Test=null;
+  dataItems=[]
   itemId = null;
   quantityId = null;
   unitTypeId = null;
   unitList = [];
   Date = null;
-  remark=''
+  remark=''; 
+   itemsId=null;
+
+
+  selectOption="In";
   quantity = [{
     name: '5',
     id: '5'
@@ -40,48 +46,74 @@ export class GotPassComponent implements OnInit {
     name: '30',
     id: '30'
   }];
+
   stateType = [{
-    name: 'Increase In Count',
-    id: '-6'
-  }, {
-    name: 'Transhipment',
-    id: '2'
-  }, {
-    name: 'out for delivery',
-    id: '4'
-  }, {
-    name: 'Damage',
-    id: '5'
-  },
-  {
-    name: 'Missing/theft',
-    id: '6'
-  }, {
-    name: 'receive Manifest',
-    id: '-1'
-  }, {
-    name: 'Received By GP',
-    id: '-2'
-  }, {
-    name: 'delivered',
-    id: '1'
-  }, {
-    name: 'received lr',
-    id: '-3'
-  }, {
-    name: 'returned Back',
-    id: '-4'
-  }]
-  StateId = null;
+    id:null,
+    name:null
+  }];
+  stateType1=[{
+    id:null,
+    name:null
+  }];
+  userInput='';
+StateId = null;
   constructor(public common: CommonService,
     public modalService: NgbModal,
     private api: ApiService,
     public activeModal: NgbActiveModal) {
     this.getUnitList();
+    this.getItems();
+    this.stateType=[{
+ 
+      name: 'Transhipment',
+      id: 2
+    }, {
+      name: 'out for delivery',
+      id: 4
+    }, {
+      name: 'Damage',
+      id: 5
+    },
+    {
+      name: 'Missing/theft',
+      id: 6
+    }, {
+      name: 'delivered',
+      id: 1 
+    }];
+    this.stateType1=[ {
+      name: 'receive Manifest',
+      id: -1
+    }, {
+      name: 'Received By GP',
+      id: -2
+    },
+      {
+     name: 'received lr',
+      id: -3
+    }, {
+      name: 'returned Back',
+      id: -4
+    },
+    {
+    name: 'Increase In Count',
+    id: -6
+  }];
   }
 
   ngOnInit() {
   }
+
+  getItems(){
+    this.api.get("WareHouse/getWhStockItem").subscribe(
+      res => {
+        this.dataItems = res['data']
+        console.log("autosugg", this.dataItems);
+
+      }
+    )
+  }
+
   getUnitList() {
     let params = {
       search: ''
@@ -97,10 +129,72 @@ export class GotPassComponent implements OnInit {
         this.common.showError();
       });
   }
+
+
   closeModal() {
     this.activeModal.close();
   }
+
   cancelRequest() {
     this.activeModal.close();
   }
-}
+
+  onSet(type){
+    this.selectOption='';
+    this.selectOption = type;
+    if(this.selectOption =='In')
+    {
+      console.log("test", this.selectOption);
+        console.log("data",this.stateType);
+        
+          return this.stateType;
+      }
+      else{
+         return this.stateType1;
+      }
+    }
+
+ 
+
+
+  getAdvice(){
+    let TDate = this.common.dateFormatter(this.Date);
+
+    const  params = {
+    stateId:this.StateId,
+    unitType:this.unitTypeId,
+    itemId:this.itemsId,
+    qty:this.quantityId,
+    dttime:TDate,
+    remarks:this.remark,
+    perDetail:this.userInput,
+      
+    }
+    console.log("result",params)
+    this.common.loading++;
+    this.api.post('WareHouse/saveWhStockItemState', params)
+      .subscribe(res => {
+        this.common.loading--;
+        this.unitList = res['data'];
+        console.log('type', this.unitList);
+        if (res['data'][0].y_id > 0) {
+
+          this.common.showToast("Sucessfully insert");
+
+          this.activeModal.close({ data: true });
+        }
+        else
+        {
+          this.common.showError("item_id,date or other detail is missing ");
+
+        }
+      }, err => {
+        this.common.showError("item_id,date or other detail is missing" );
+        console.log('Error: ', err);
+      });
+  }
+
+
+  
+  }
+
--- src/app/ware-house/warehouse-inventory/warehouse-inventory.component.ts
+++ src/app/ware-house/warehouse-inventory/warehouse-inventory.component.ts
@@ -27,6 +27,7 @@ export class WarehouseInventoryComponent implements OnInit {
   constructor(public common: CommonService,
     public modalService: NgbModal,
     private api: ApiService) {
+      this.getdata();
 
   }
 
@@ -35,30 +36,77 @@ export class WarehouseInventoryComponent implements OnInit {
   manualWareHouseEntry() {
     this.modalService.open(GotPassComponent, { size: 'lg', container: 'nb-layout', backdrop: 'static' });
   }
-  nearByPods() {
-    this.table = {
-      data: {
-        headings: {},
-        columns: []
-      },
-      settings: {
-        hideHeader: true
+
+  // nearByPods() {
+  //   this.table = {
+  //     data: {
+  //       headings: {},
+  //       columns: []
+  //     },
+  //     settings: {
+  //       hideHeader: true
+  //     }
+  //   };
+
+  //   this.headings = [];
+  //   this.valobj = {};
+
+
+  //   this.common.loading++;
+  //   this.api.get('LorryReceiptsOperation/getNearByPods')
+  //     .subscribe(res => {
+  //       this.common.loading--;
+  //       this.data = res['data'];
+  //       if (this.data == null) {
+  //         this.data = [];
+  //         this.table = null;
+  //       }
+  //       let first_rec = this.data[0];
+  //       for (var key in first_rec) {
+  //         if (key.charAt(0) != "_") {
+  //           this.headings.push(key);
+  //           let headerObj = { title: this.formatTitle(key), placeholder: this.formatTitle(key) };
+  //           this.table.data.headings[key] = headerObj;
+  //         }
+  //       }
+  //       // this.showdoughnut();
+  //       this.table.data.columns = this.getTableColumns();
+
+  //     }, err => {
+  //       this.common.loading--;
+  //       this.common.showError();
+  //     });
+  // }
+
+  formatTitle(title) {
+    return title.charAt(0).toUpperCase() + title.slice(1)
+  }
+
+   getTableColumns() {
+    let columns = [];
+    console.log("Data=", this.data);
+    this.data.map(doc => {
+      this.valobj = {};
+      for (let i = 0; i < this.headings.length; i++) {
+        console.log("doc index value:", doc[this.headings[i]]);
+          this.valobj['Action'] = { class: "fas fa-eye", action: this.showAction.bind(this, doc.status, doc.foid) }
+        
+        this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action: '' };
+      
       }
-    };
+      columns.push(this.valobj);
+    });
+    return columns;
+  }
 
-    this.headings = [];
-    this.valobj = {};
-
-
-    this.common.loading++;
-    this.api.get('LorryReceiptsOperation/getNearByPods')
-      .subscribe(res => {
-        this.common.loading--;
+  getdata(){
+  
+    console.log("ap")
+    this.api.get("WareHouse/getStockItemPendingList").subscribe(
+      res => {
+        this.data = [];
         this.data = res['data'];
-        if (this.data == null) {
-          this.data = [];
-          this.table = null;
-        }
+        console.log("result", res);
         let first_rec = this.data[0];
         for (var key in first_rec) {
           if (key.charAt(0) != "_") {
@@ -67,35 +115,16 @@ export class WarehouseInventoryComponent implements OnInit {
             this.table.data.headings[key] = headerObj;
           }
         }
-        // this.showdoughnut();
         this.table.data.columns = this.getTableColumns();
-
-      }, err => {
-        this.common.loading--;
-        this.common.showError();
-      });
-  }
-  formatTitle(title) {
-    return title.charAt(0).toUpperCase() + title.slice(1)
-  }
-  getTableColumns() {
-    let columns = [];
-
-    this.data.map(doc => {
-      this.valobj = {};
-      for (let i = 0; i < this.headings.length; i++) {
-        // console.log("Type", this.headings[i]);
-        // console.log("doc index value:", doc[this.headings[i]]);
-        if (this.headings[i] == "Action") {
-
-          // this.valobj[this.headings[i]] = { value: "", action: null, icons: [{ class: 'fa fa-task', action: this.view.bind(this, doc.url) }, { class: 'fa fa-task', action: this.view.bind(this, doc.url) }] };
-          // this.valobj[this.headings[i]] = { value: "", action: null, icons: [{ class: 'fa fa-user', action: this.change.bind(this, doc) }, { class: 'fa fa-map-marker', action: this.view.bind(this, doc) }, { class: 'fa fa-picture-o', action: this.LRimage.bind(this, doc._img_url2) }, { class: 'fa fa-image', action: this.podimage.bind(this, doc._img_url) }] };
-        } else {
-          this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action: '' };
-        }
       }
-      columns.push(this.valobj);
+    );
+  }
+
+  showAction() {
+    // this.common.params = {warehouseId : this.wareHouseId};
+    const activeModal = this.modalService.open(GotPassComponent, {
+      size: "lg",
+      container: "nb-layout"
     });
-    return columns;
   }
 }
