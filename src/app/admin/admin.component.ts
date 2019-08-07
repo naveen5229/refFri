040fceb0af2fcf21964749266f474751f390c716
Lalit <singhlalit843@gmail.com>
2019-08-07 11:30:41 +0530

    chnage in user-assign

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,6 +9,7 @@ import { UserService } from '../services/user.service';
 import { CommonService } from '../services/common.service';
 import { Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'ngx-pages',
@@ -24,6 +25,7 @@ export class AdminComponent {
   menu = this.common.menuGenerator('admin');
   constructor(public common: CommonService,
     public user: UserService,
+    public api: ApiService,
     public router: Router,
     public accountService: AccountService) {
 
@@ -32,5 +34,33 @@ export class AdminComponent {
       this.router.navigate(['/pages']);
       return;
     }
+    if (!this.accountService.branches.length) {
+      this.getBranches();
+      this.getFinancial();
+    }
+  }
+  getBranches() {
+    this.api.post('Suggestion/GetBranchList', { search: 123 })
+      .subscribe(res => {
+        console.log('Branches :', res['data']);
+        this.accountService.branches = res['data'];
+      }, err => {
+        console.log('Error: ', err);
+      });
+  }
+
+  getFinancial() {
+    this.api.post('Suggestion/GetFinancialYear', { search: 123 })
+      .subscribe(res => {
+        console.log('financial :', res['data']);
+        this.accountService.financialYears = res['data'];
+        this.accountService.financialYears.map(financialYear => {
+          if (financialYear.name.split('-')[0] == (new Date()).getFullYear()) {
+            this.accountService.selected.financialYear = financialYear;
+          }
+        });
+      }, err => {
+        console.log('Error: ', err);
+      });
   }
 }
