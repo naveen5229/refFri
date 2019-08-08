7298d75ba270ffa57367cb127cea03012fa8a5e8
unknown <sachinelogic@gmail.com>
2019-08-08 18:02:45 +0530

    tally Export page

--- src/app/accounts/tallyexport/tallyexport.component.ts
+++ src/app/accounts/tallyexport/tallyexport.component.ts
@@ -18,6 +18,7 @@ import 'rxjs/add/operator/toPromise';
 // import { saveAs } from 'file-saver/FileSaver';
 import { saveAs } from 'file-saver';
 import { ExcelService } from '../../services/excel/excel.service';
+import { constructor } from 'vfile-message';
 
 
 @Component({
@@ -28,6 +29,13 @@ import { ExcelService } from '../../services/excel/excel.service';
 export class TallyexportComponent implements OnInit {
   headingName = '';
   branchname='';
+  branchData=[];
+  branchType={
+    name:'',
+    id:0
+  }
+  endDate= this.common.dateFormatternew(new Date(), 'ddMMYYYY', false, '-');
+  startDate= this.common.dateFormatternew(new Date(), 'ddMMYYYY', false, '-');
   constructor(public api: ApiService,
     public common: CommonService,
     public user: UserService,
@@ -41,6 +49,11 @@ export class TallyexportComponent implements OnInit {
     this.common.currentPage = 'Tally Export';
     this.headingName = this.user._customer.name;
     this.branchname = this.accountService.selected.branch.name;
+    setTimeout(() => {
+    this.branchData=this.accountService.branches;
+    console.log('branchesw',this.branchData);
+    this.setFoucus('branchType');
+    }, 3000);
   }
 
   activeGroup = [];
@@ -52,10 +65,36 @@ export class TallyexportComponent implements OnInit {
   }
 
 
+  onSelected(selectedData, type, display) {
+    console.log('selected data',selectedData);
+    this.branchType.name = selectedData[display];
+    this.branchType.id = selectedData.id;
+    // console.log('order User: ', this.DayBook);
+  }
+
+  
+  keyHandler(event) {
+    const key = event.key.toLowerCase();
+    let activeId = document.activeElement.id;
+    console.log('Active event', event);
+    if (key == 'enter') {
+     // this.allowBackspace = true;
+      if (activeId.includes('branchType')) {
+        this.setFoucus('startDate');
+      } else if (activeId.includes('startDate')) {
+        this.setFoucus('endDate');
+      }else if (activeId.includes('endDate')) {
+        this.setFoucus('master');
+      }
+  }
+}
   getTallyData(type,filename) {
     return new Promise((resolve, reject) => {
       const params = {
         tallyType: type,
+        branchid: this.branchType.id,
+        startDate:this.startDate,
+        endDate:this.endDate
       };
       this.common.loading++;
       this.api.post('Voucher/getTallyData', params)
@@ -73,5 +112,12 @@ export class TallyexportComponent implements OnInit {
     });
   }
   
+  setFoucus(id, isSetLastActive = true) {
+    setTimeout(() => {
+      let element = document.getElementById(id);
+      console.log('Element: ', element);
+      element.focus();
+    }, 100);
+  }
 }
 
