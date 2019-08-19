3249de1c73e59e988f32f369d72149edb06c6936
Lalit <singhlalit843@gmail.com>
2019-08-19 14:54:01 +0530

    chnages in edit fuel filling modal

--- src/app/modals/vehicle-time-table-association/vehicle-time-table-association.component.ts
+++ src/app/modals/vehicle-time-table-association/vehicle-time-table-association.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { CommonService } from '../../services/common.service';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmComponent } from '../confirm/confirm.component';
 
 @Component({
   selector: 'vehicle-time-table-association',
@@ -22,6 +23,17 @@ export class VehicleTimeTableAssociationComponent implements OnInit {
       name: null,
     }];
   vehicleId = null;
+  table = {
+    data: {
+      headings: {},
+      columns: []
+    },
+    settings: {
+      hideHeader: true
+    }
+  };
+
+  vehicleTimeTable = [];
 
   constructor(public api: ApiService,
     public common: CommonService,
@@ -139,6 +151,130 @@ export class VehicleTimeTableAssociationComponent implements OnInit {
         console.log(err);
       });
   }
+
+
+
+  getVehicleRouteTimeTable() {
+    this.common.loading++;
+    this.api.get('ViaRoutes/getTimeTable?routeId=' + this.routeId)
+      .subscribe(res => {
+        this.common.loading--;
+        this.vehicleTimeTable = res['data'];
+        this.vehicleTimeTable.length ? this.setTable() : this.resetTable();
+
+      }, err => {
+        this.common.loading--;
+        console.log(err);
+      });
+  }
+
+
+  resetTable() {
+    this.table.data = {
+      headings: {},
+      columns: []
+    };
+  }
+
+  setTable() {
+    this.table.data = {
+      headings: this.generateHeadings(),
+      columns: this.getTableColumns()
+    };
+    return true;
+  }
+
+  generateHeadings() {
+    let headings = {};
+    for (var key in this.vehicleTimeTable[0]) {
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: key, placeholder: this.formatTitle(key) };
+      }
+    }
+    return headings;
+  }
+
+  getTableColumns() {
+    let columns = [];
+    this.vehicleTimeTable.map(route => {
+      let column = {};
+      for (let key in this.generateHeadings()) {
+        if (key == 'Action') {
+          column[key] = {
+            value: "",
+            isHTML: false,
+            action: null,
+            icons: this.actionIcons(route)
+          };
+        } else {
+          column[key] = { value: route[key], class: 'black', action: '' };
+        }
+      }
+      columns.push(column);
+    })
+
+    return columns;
+  }
+
+  actionIcons(route) {
+    let icons = [
+      {
+        class: "fas fa-trash-alt",
+        action: this.deleteRouteTime.bind(this, route)
+      },
+    ];
+    return icons;
+  }
+
+  formatTitle(strval) {
+    let pos = strval.indexOf('_');
+    if (pos > 0) {
+      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+    } else {
+      return strval.charAt(0).toUpperCase() + strval.substr(1);
+    }
+  }
+
+
+
+
+
+  deleteRouteTime(route) {
+    let params = {
+      routeId: route._route_id,
+      timeTableId: route._rtt_id,
+
+    }
+    if (route._route_id) {
+      this.common.params = {
+        title: 'Delete Vehicle Route Time',
+        description: `<b>&nbsp;` + 'Are Sure To Delete This Record' + `<b>`,
+      }
+      const activeModal = this.modalService.open(ConfirmComponent, { size: 'sm', container: 'nb-layout', backdrop: 'static', keyboard: false, windowClass: "accountModalClass" });
+      activeModal.result.then(data => {
+        if (data.response) {
+          console.log("data", data);
+          this.common.loading++;
+          this.api.post('ViaRoutes/deleteTimeTable', params)
+            .subscribe(res => {
+              this.common.loading--;
+              if (res['data'][0].y_id > 0) {
+                this.common.showToast('Success');
+                this.getVehicleRouteTimeTable();
+              }
+              else {
+                this.common.showToast(res['data'][0].y_msg);
+              }
+
+            }, err => {
+              this.common.loading--;
+              console.log('Error: ', err);
+            });
+        }
+      });
+    }
+  }
+
 
 
 }
