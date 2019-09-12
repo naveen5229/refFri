a2b711923fad4226b359da733af30a79758f68ec
unknown <sachinelogic@gmail.com>
2019-09-12 19:24:59 +0530

    test voucher

--- src/app/accounts-modals/voucher-summary/voucher-summary.component.ts
+++ src/app/accounts-modals/voucher-summary/voucher-summary.component.ts
@@ -1126,7 +1126,7 @@ export class VoucherSummaryComponent implements OnInit {
       { txt: this.totalRevinue || '' },
       { txt: this.totalFuel || '' },
       { txt: this.alltotal || '' },
-      { txt: this.alltotal || '' },
+      { txt: this.totalRevinue - (this.alltotal + this.totalFuel) || '' },
 
 
 
