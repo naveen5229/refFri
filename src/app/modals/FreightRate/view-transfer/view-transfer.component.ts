ea0702bdba6dd193f3ca31ee10b2cbfce661a2b5
Hemant <hemant1.axestrack@gmail.com>
2019-09-18 16:49:52 +0530

    create print functionality multiple pages

--- src/app/modals/FreightRate/view-transfer/view-transfer.component.ts
+++ src/app/modals/FreightRate/view-transfer/view-transfer.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../../../services/api.service';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmComponent } from '../../confirm/confirm.component';
 import { UserService } from '../../../services/user.service';
+import { PrintService } from '../../../services/print/print.service';
 
 @Component({
   selector: 'view-transfer',
@@ -14,9 +15,9 @@ export class ViewTransferComponent implements OnInit {
 
   startTime = new Date(new Date().setDate(new Date().getDate() - 30));;
   endTime = new Date();
-  transferType=-1;
-  ledgerId='';
-  ledgerType='';
+  transferType = -1;
+  ledgerId = '';
+  ledgerType = '';
 
   data = [];
   table = {
@@ -30,7 +31,7 @@ export class ViewTransferComponent implements OnInit {
   };
   headings = [];
   valobj = {};
-
+  params = null;
 
   constructor(
     public common: CommonService,
@@ -38,12 +39,12 @@ export class ViewTransferComponent implements OnInit {
     public activeModal: NgbActiveModal,
     public renderer: Renderer,
     public modalService: NgbModal,
-    public user:UserService,
-  ) 
-  { 
-    this.ledgerId=this.common.params.ledgerId;
-    this.ledgerType="Ledger:"+this.common.params.title;
-    console.log("ledgerType:",this.ledgerType);
+    public printService: PrintService,
+    public user: UserService) {
+    this.params = this.common.params;
+    this.ledgerId = this.common.params.ledgerId;
+    this.ledgerType = "Ledger:" + this.common.params.title;
+    console.log("ledgerType:", this.ledgerType);
     this.common.handleModalSize('class', 'modal-lg', '1400');
     this.viewTransfer();
   }
@@ -51,10 +52,9 @@ export class ViewTransferComponent implements OnInit {
   ngOnInit() {
   }
 
-  viewTransfer()
-  {
+  viewTransfer() {
     const params = "startTime=" + this.common.dateFormatter(this.startTime) +
-      "&endTime=" + this.common.dateFormatter(this.endTime)+"&ledgerId="+this.ledgerId;
+      "&endTime=" + this.common.dateFormatter(this.endTime) + "&ledgerId=" + this.ledgerId;
     ++this.common.loading;
 
     this.api.get('FrieghtRate/getLedgerTransfers?' + params)
@@ -91,20 +91,17 @@ export class ViewTransferComponent implements OnInit {
         console.log('Error: ', err);
       });
   }
+
   getTableColumns() {
     let columns = [];
     console.log("Data=", this.data);
     this.data.map(doc => {
       this.valobj = {};
       for (let i = 0; i < this.headings.length; i++) {
-        
-
-          console.log("doc index value:", doc[this.headings[i]]);
-          this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action: '' };
-        
+        console.log("doc index value:", doc[this.headings[i]]);
+        this.valobj[this.headings[i]] = { value: doc[this.headings[i]], class: 'black', action: '' };
       }
       columns.push(this.valobj);
-
     });
 
     return columns;
@@ -114,46 +111,10 @@ export class ViewTransferComponent implements OnInit {
     return title.charAt(0).toUpperCase() + title.slice(1);
   }
 
-
-  // deleteTransfer(row) {
-  //   console.log("row", row);
-  //   let params = {
-  //     id: row._id,
-  //   }
-  //   if (row._id) {
-  //     this.common.params = {
-  //       title: 'Delete Route ',
-  //       description: `<b>&nbsp;` + 'Are Sure To Delete This Record' + `<b>`,
-  //     }
-  //     const activeModal = this.modalService.open(ConfirmComponent, { size: 'sm', container: 'nb-layout', backdrop: 'static', keyboard: false, windowClass: "accountModalClass" });
-  //     activeModal.result.then(data => {
-  //       if (data.response) {
-  //         console.log("data", data);
-  //         this.common.loading++;
-  //         this.api.post('FrieghtRate/deleteTransfers', params)
-  //           .subscribe(res => {
-  //             this.common.loading--;
-  //             if (res['data'][0].y_id > 0) {
-  //               this.common.showToast('Success');
-  //               this.viewTransfer();
-  //             }
-  //             else {
-  //               this.common.showToast(res['data'][0].y_msg);
-  //             }
-  //           }, err => {
-  //             this.common.loading--;
-  //             console.log('Error: ', err);
-  //           });
-  //       }
-  //     });
-  //   }
-  // }
-
   closeModal() {
     this.activeModal.close();
   }
 
-  
   printPDF(tblEltId) {
     this.common.loading++;
     let userid = this.user._customer.id;
@@ -162,7 +123,7 @@ export class ViewTransferComponent implements OnInit {
     this.api.post('FoAdmin/getFoDetailsFromUserId', { x_user_id: userid })
       .subscribe(res => {
         this.common.loading--;
-        console.log("Api data",res['data']);
+        console.log("Api data", res['data']);
         let fodata = res['data'];
         let left_heading = fodata['name'];
         let center_heading = "View Transfer";
@@ -174,27 +135,34 @@ export class ViewTransferComponent implements OnInit {
   }
 
   printHandler() {
-    this.renderer.setElementClass(document.body, 'test', true);
-    let css = '@page { size: landscape !important; }';
-    let head = document.head || document.getElementsByTagName('head')[0];
-    let style = document.createElement('style');
-
-    style.type = 'text/css';
-    style.media = 'print';
-
-    if (style['styleSheet']) {
-      style['styleSheet'].cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    head.appendChild(style);
-    window.print();
-    let printWindowListener = setInterval(() => {
-      if (document.readyState == "complete") {
-        clearInterval(printWindowListener);
-        head.removeChild(style);
-        this.renderer.setElementClass(document.body, 'test', false);
-      }
-    }, 1000);
+    let rows = [];
+    this.data.map((transfer, index) => {
+      let column = [];
+      column.push({txt: index + 1});
+      this.headings.forEach(heading => column.push({ txt: transfer[heading] !== null ? transfer[heading] : '' }));
+      rows.push(column);
+    });
+    let xJSON1 = {
+      "headers":
+        [
+          { "txt": this.params.title, size: '22px', weight: 'bold' },
+        ],
+      "details": [],
+      tables: [{
+        "headings":
+          [{ "txt": "#" }, { "txt": "Time" }, { "txt": "Payment For" }, { "txt": "Party" },
+          { "txt": "Receipt" }, { "txt": "Transfer" }, { "txt": "Balance" }, { "txt": "Remarks" }],
+        "rows": rows,
+        name: ''
+      }],
+      "signatures": [],
+      "footer": {
+        "left": { "name": "Powered By", "value": "Elogist Solutions" },
+        "center": { "name": "Printed Date", "value": this.common.dateFormatternew(new Date(), 'ddMMYYYY').split(' ')[0] },
+        "right": { "name": "Page No", "value": 1 }
+      },
+      footertotal: []
+    };
+    this.printService.generalPrint(xJSON1);
   }
 }
