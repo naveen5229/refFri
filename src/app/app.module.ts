d545ffaf8c1f5eb60c6ad2dd7ac155f3fb6bedf0
unknown <prashant.sharma@elogist.in>
2019-09-20 18:43:18 +0530

    transfer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -248,6 +248,7 @@ import { ViewMVSFreightStatementComponent } from './modals/FreightRate/view-mvsf
 import { TemplateDevviewComponent } from './modals/template-devview/template-devview.component';
 import { MarketVehFreightStatementComponent } from './modals/FreightRate/market-veh-freight-statement/market-veh-freight-statement.component';
 import { MvsLrAssignComponent } from './modals/FreightRate/mvs-lr-assign/mvs-lr-assign.component';
+import { ManifestGenerateComponent } from './modals/LRModals/manifest-generate/manifest-generate.component';
 
 
 
@@ -469,6 +470,7 @@ import { MvsLrAssignComponent } from './modals/FreightRate/mvs-lr-assign/mvs-lr-
     TemplateDevviewComponent,
     MarketVehFreightStatementComponent,
     MvsLrAssignComponent,
+    ManifestGenerateComponent,
 
   ],
   entryComponents: [
