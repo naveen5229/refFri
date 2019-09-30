4cf3194734ebcae34e490777f686ae509df96eca
Lalit Singh <lalit.singh@elogist.in>
2019-09-30 18:42:59 +0530

    changes in un-merge

--- src/app/modals/un-merge-state/un-merge-state.component.ts
+++ src/app/modals/un-merge-state/un-merge-state.component.ts
@@ -24,6 +24,8 @@ export class UnMergeStateComponent implements OnInit {
     time: ''
   };
   unMergeEvents = [];
+  vehicleEventsR = [];
+
   constructor(public modalService: NgbModal,
     public common: CommonService,
     public api: ApiService,
@@ -84,48 +86,48 @@ export class UnMergeStateComponent implements OnInit {
         // this.clearAllMarkers();
         // this.createMarkers(res['data']);
         // this.resetBtnStatus();
-        // // ------------------------ Route Mapper Code (Authored by UJ) ----------------------
-        // let startElement = this.vehicleEvents.find((element) => {
-        //   return !(element.desc + "").includes('LT');
-        // });
-        // this.vehicleEvents.forEach((element) => {
-        //   if ((element.haltTypeId == 21 || element.haltTypeId == 11)
-        //     && (element.desc + "").includes('LT'))
-        //     element.lastType = element.haltTypeId;
-        //   else
-        //     element.lastType = null;
-        // });
-        // console.log("StartElement", startElement);
-        // if (startElement) {
-        //   let start = startElement.startTime;
-        //   let startIndex = this.vehicleEvents.indexOf(startElement);
-        //   let end = this.vehicleEvents[this.vehicleEvents.length - 1].endTime;
-        //   console.log(res);
-        //   this.vehicleEventsR = [];
-        //   let vehicleEvents = res['data'];
-        //   let realStart = new Date(vehicleEvents[startIndex].startTime) < new Date(start) ?
-        //     vehicleEvents[startIndex].startTime : start;
-        //   let realEnd = null;
-        //   if (vehicleEvents[0].endTime)
-        //     realEnd = new Date(vehicleEvents[vehicleEvents.length - 1].endTime) > new Date(end) ?
-        //       vehicleEvents[vehicleEvents.length - 1].endTime : end;
-        //   let totalHourDiff = 0;
-        //   if (vehicleEvents.length != 0) {
-        //     totalHourDiff = this.common.dateDiffInHours(realStart, realEnd, true);
-        //     console.log("Total Diff", totalHourDiff);
-        //   }
-        //   for (let index = startIndex; index < vehicleEvents.length; index++) {
-        //     vehicleEvents[index].mIndex = index;
-        //     startIndex++;
-        //     vehicleEvents[index].position = (this.common.dateDiffInHours(
-        //       realStart, vehicleEvents[index].startTime) / totalHourDiff) * 98;
-        //     vehicleEvents[index].width = (this.common.dateDiffInHours(
-        //       vehicleEvents[index].startTime, vehicleEvents[index].endTime, true) / totalHourDiff) * 98;
-        //     console.log("Width", vehicleEvents[index].width);
-        //     this.vehicleEventsR.push(vehicleEvents[index]);
-        //   }
-        //   console.log("VehicleEvents", this.vehicleEventsR);
-        // }
+        // ------------------------ Route Mapper Code (Authored by UJ) ----------------------
+        let startElement = this.unMergeEvents.find((element) => {
+          return !(element.desc + "").includes('LT');
+        });
+        this.unMergeEvents.forEach((element) => {
+          if ((element.haltTypeId == 21 || element.haltTypeId == 11)
+            && (element.desc + "").includes('LT'))
+            element.lastType = element.haltTypeId;
+          else
+            element.lastType = null;
+        });
+        console.log("StartElement", startElement);
+        if (startElement) {
+          let start = startElement.startTime;
+          let startIndex = this.unMergeEvents.indexOf(startElement);
+          let end = this.unMergeEvents[this.unMergeEvents.length - 1].endTime;
+          console.log(res);
+          this.vehicleEventsR = [];
+          let unMergeEvents = res['data'];
+          let realStart = new Date(unMergeEvents[startIndex].startTime) < new Date(start) ?
+            unMergeEvents[startIndex].startTime : start;
+          let realEnd = null;
+          if (unMergeEvents[0].endTime)
+            realEnd = new Date(unMergeEvents[unMergeEvents.length - 1].endTime) > new Date(end) ?
+              unMergeEvents[unMergeEvents.length - 1].endTime : end;
+          let totalHourDiff = 0;
+          if (unMergeEvents.length != 0) {
+            totalHourDiff = this.common.dateDiffInHours(realStart, realEnd, true);
+            console.log("Total Diff", totalHourDiff);
+          }
+          for (let index = startIndex; index < unMergeEvents.length; index++) {
+            unMergeEvents[index].mIndex = index;
+            startIndex++;
+            unMergeEvents[index].position = (this.common.dateDiffInHours(
+              realStart, unMergeEvents[index].startTime) / totalHourDiff) * 98;
+            unMergeEvents[index].width = (this.common.dateDiffInHours(
+              unMergeEvents[index].startTime, unMergeEvents[index].endTime, true) / totalHourDiff) * 98;
+            console.log("Width", unMergeEvents[index].width);
+            this.vehicleEventsR.push(unMergeEvents[index]);
+          }
+          console.log("unMergeEvents", this.vehicleEventsR);
+        }
 
 
 
@@ -135,4 +137,41 @@ export class UnMergeStateComponent implements OnInit {
         console.log(err);
       })
   }
+
+  // openSmartTool(i, vehicleEvent) {
+  //   if (this. != null && this.hsId != vehicleEvent.haltId && vehicleEvent.haltId != null)
+  //     if (confirm("Merge with this Halt?")) {
+  //       this.common.loading++;
+  //       let params = { ms_id: this.vSId, hs_id: vehicleEvent.vs_id };
+  //       console.log("params", params);
+  //       this.api.post('HaltOperations/mergeManualStates', params)
+  //         .subscribe(res => {
+  //           this.common.loading--;
+  //           if (res['success']) {
+  //             this.reloadData();
+  //           } else {
+  //             this.common.showToast(res['msg']);
+  //             this.reloadData();
+  //           }
+  //         }, err => {
+  //           this.common.loading--;
+  //           this.common.showError(err);
+  //           console.log(err);
+  //         });
+  //     }
+  //   this.vSId = null;
+  //   this.isChecks = {};
+  //   console.log(this.onlyDrag);
+  //   if (!this.onlyDrag) {
+  //     this.unMergeEvents.forEach(vEvent => {
+  //       if (vEvent != vehicleEvent)
+  //         vEvent.isOpen = false;
+  //     });
+  //     vehicleEvent.isOpen = !vehicleEvent.isOpen;
+  //     this.zoomFunctionality(i, vehicleEvent);
+  //     this.getSites();
+  //   }
+  //   this.onlyDrag = false;
+  // }
+
 }
