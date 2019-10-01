3b1ce85deb48ce2244488bacd31822cc7def7b68
Lalit Singh <lalit.singh@elogist.in>
2019-10-01 12:54:03 +0530

    changes in un merge state

--- src/app/modals/un-merge-state/un-merge-state.component.ts
+++ src/app/modals/un-merge-state/un-merge-state.component.ts
@@ -25,7 +25,22 @@ export class UnMergeStateComponent implements OnInit {
   };
   unMergeEvents = [];
   vehicleEventsR = [];
+  Markers = [];
+  designsDefaults = [
+    "M  0,0,  0,-5,  -5,-5,-5,-13 , 5,-13 ,5,-5, 0,-5 z",///Rect
+    "M  0,0,  0,-5,  -5,-13 , 5,-13 , 0,-5 z",//Pin
+    "M  0,0,  0,-5,  -5,-13 , 5,-13 , 0,-5 z"///Rect
+  ];
+  bounds = null;
+  infowindow = null;
+  btnStatus = true;
+  vSId = null;
+  isChecks = {};
+  hsId: any;
+  onlyDrag = false;
+  lastIndDetails = null;
 
+  polygons = [];
   constructor(public modalService: NgbModal,
     public common: CommonService,
     public api: ApiService,
@@ -83,9 +98,9 @@ export class UnMergeStateComponent implements OnInit {
         this.common.loading--;
         console.log("Api result,", res);
         this.unMergeEvents = res['data'];
-        // this.clearAllMarkers();
-        // this.createMarkers(res['data']);
-        // this.resetBtnStatus();
+        this.clearAllMarkers();
+        this.createMarkers(res['data']);
+        this.resetBtnStatus();
         // ------------------------ Route Mapper Code (Authored by UJ) ----------------------
         let startElement = this.unMergeEvents.find((element) => {
           return !(element.desc + "").includes('LT');
@@ -138,40 +153,268 @@ export class UnMergeStateComponent implements OnInit {
       })
   }
 
-  // openSmartTool(i, vehicleEvent) {
-  //   if (this. != null && this.hsId != vehicleEvent.haltId && vehicleEvent.haltId != null)
-  //     if (confirm("Merge with this Halt?")) {
-  //       this.common.loading++;
-  //       let params = { ms_id: this.vSId, hs_id: vehicleEvent.vs_id };
-  //       console.log("params", params);
-  //       this.api.post('HaltOperations/mergeManualStates', params)
-  //         .subscribe(res => {
-  //           this.common.loading--;
-  //           if (res['success']) {
-  //             this.reloadData();
-  //           } else {
-  //             this.common.showToast(res['msg']);
-  //             this.reloadData();
-  //           }
-  //         }, err => {
-  //           this.common.loading--;
-  //           this.common.showError(err);
-  //           console.log(err);
-  //         });
-  //     }
-  //   this.vSId = null;
-  //   this.isChecks = {};
-  //   console.log(this.onlyDrag);
-  //   if (!this.onlyDrag) {
-  //     this.unMergeEvents.forEach(vEvent => {
-  //       if (vEvent != vehicleEvent)
-  //         vEvent.isOpen = false;
-  //     });
-  //     vehicleEvent.isOpen = !vehicleEvent.isOpen;
-  //     this.zoomFunctionality(i, vehicleEvent);
-  //     this.getSites();
-  //   }
-  //   this.onlyDrag = false;
-  // }
+
+  clearAllMarkers(reset = true, boundsReset = true) {
+    for (let i = 0; i < this.Markers.length; i++) {
+      this.Markers[i].setMap(null);
+    }
+    if (reset)
+      this.Markers = [];
+    if (boundsReset) {
+      this.markerZoomMF(null, 12, true);
+    }
+  }
+  markerZoomMF(id, zoomLevel = 19, isReset = false) {
+    let latLng = !isReset ? this.Markers[id].position : new google.maps.LatLng(25, 75);
+    this.map.setCenter(latLng);
+    this.setZoom(zoomLevel);
+  }
+  setZoom(zoom) {
+    this.map.setZoom(zoom);
+    if (zoom >= 15) {
+      this.map.setMapTypeId('hybrid');
+    } else {
+      this.map.setMapTypeId('roadmap');
+    }
+  }
+  createMarkers(markers, changeBounds = true, drawPoly = false) {
+
+    let thisMarkers = [];
+    console.log("Markers", markers);
+    this.bounds = new google.maps.LatLngBounds();
+    for (let index = 0; index < markers.length; index++) {
+
+      let subType = markers[index]["subType"];
+      let design = markers[index]["type"] == "site" ? this.designsDefaults[0] :
+        markers[index]["type"] == "subSite" ? this.designsDefaults[1] : null;//this.designsDefaults[2]
+      let text = markers[index]["text"] ? markers[index]["text"] : index + 1;
+      let pinColor = markers[index]["color"] ? markers[index]["color"] : "FFFF00";
+      let lat = markers[index]["lat"] ? markers[index]["lat"] : 25;
+      let lng = markers[index]["long"] ? markers[index]["long"] : 75;
+      let title = markers[index]["title"] ? markers[index]["title"] : "Untitled";
+      let latlng = new google.maps.LatLng(lat, lng);
+      let pinImage;
+      //pin Image  
+      if (design) {
+        pinImage = {
+          path: design,
+          // set custom fillColor on each iteration
+          fillColor: "#" + pinColor,
+          fillOpacity: 1,
+          scale: 1.3,
+          strokeColor: pinColor,
+          strokeWeight: 2,
+        };
+      } else {
+        if (subType == 'marker') {
+          pinImage = "http://chart.apis.google.com/chart?chst=d_map_xpin_letter&chld=pin|" + text + "|" + pinColor + "|000000";
+          console.log("Pin Image:", pinImage);
+
+        }
+        else //if(subType=='circle')
+          pinImage = {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 3,
+            fillColor: "#" + pinColor,
+            fillOpacity: 0.8,
+            strokeWeight: 1
+          };
+      }
+
+
+      let marker = new google.maps.Marker({
+        position: latlng,
+        flat: true,
+        icon: pinImage,
+        map: this.map,
+        title: title
+      });
+      if (changeBounds && !('' + markers[index]['desc']).endsWith('LT'))
+        this.setBounds(latlng);
+      thisMarkers.push(marker);
+      console.log("ThisMarker: ", thisMarkers);
+
+      this.Markers.push(marker);
+
+      if (markers[index]["type"] == "site") {
+        let show = markers[index]['name'] + " , " + markers[index]['typename'] + " , " + markers[index]['id'];
+        marker.addListener('mouseover', this.showInfoWindow.bind(this, show, marker));
+        marker.addListener('mouseout', this.closeInfoWindow.bind(this));
+        // marker.addListener('click', this.convertSiteHalt.bind(this, markers[index]['id']));
+
+      }
+
+    }
+    return thisMarkers;
+  }
+
+  setBounds(latlng) {
+    if (!this.bounds)
+      this.bounds = this.map.getBounds();
+    this.bounds.extend(latlng);
+    this.map.fitBounds(this.bounds);
+  }
+  showInfoWindow(show, marker) {
+    console.log('Info:', show);
+    if (this.infowindow != null) {
+      this.infowindow.close();
+    }
+
+    this.infowindow = new google.maps.InfoWindow({
+      content: show,
+      size: new google.maps.Size(50, 50),
+      maxWidth: 300
+    });
+
+    this.infowindow.open(this.map, marker);
+  }
+
+  toggleBounceMF(id, evtype = 1) {
+    //console.log("Bounce marker",id);
+    //console.log("index",index);
+    //.log("test",test);
+    //console.log("item",item);
+    if (this.Markers[id]) {
+      if (this.Markers[id].getAnimation() == null && evtype == 1) {
+        this.Markers[id].setAnimation(google.maps.Animation.BOUNCE);
+      }
+      else if (evtype == 2 && this.Markers[id].getAnimation() != null) {
+        this.Markers[id].setAnimation(null);
+      }
+    }
+  }
+
+  closeInfoWindow() {
+    this.infowindow.close();
+  }
+
+  resetBtnStatus() {
+    this.btnStatus = true;
+    this.unMergeEvents.forEach(vehicleEventDetail => {
+      console.log("vehicleEventDetail", vehicleEventDetail)
+      if (vehicleEventDetail.color == 'ff13ec') {
+        this.btnStatus = false;
+        return;
+      }
+    });
+  }
+
+  openSmartTool(i, vehicleEvent) {
+    if (this.vSId != null && this.hsId != vehicleEvent.haltId && vehicleEvent.haltId != null)
+      // if (confirm("Merge with this Halt?")) {
+      //   this.common.loading++;
+      //   let params = { ms_id: this.vSId, hs_id: vehicleEvent.vs_id };
+      //   console.log("params", params);
+      //   this.api.post('HaltOperations/mergeManualStates', params)
+      //     .subscribe(res => {
+      //       this.common.loading--;
+      //       if (res['success']) {
+      //         this.reloadData();
+      //       } else {
+      //         this.common.showToast(res['msg']);
+      //         this.reloadData();
+      //       }
+      //     }, err => {
+      //       this.common.loading--;
+      //       this.common.showError(err);
+      //       console.log(err);
+      //     });
+      // }
+      this.vSId = null;
+    this.isChecks = {};
+    console.log(this.onlyDrag);
+    if (!this.onlyDrag) {
+      this.unMergeEvents.forEach(vEvent => {
+        if (vEvent != vehicleEvent)
+          vEvent.isOpen = false;
+      });
+      vehicleEvent.isOpen = !vehicleEvent.isOpen;
+      this.zoomFunctionality(i, vehicleEvent);
+      // this.getSites();
+    }
+    this.onlyDrag = false;
+  }
+
+  zoomFunctionality(i, vehicleEvent) {
+    console.log("vehicleEvent", vehicleEvent);
+    this.markerZoomMF(i, 19);
+    console.log(">>>>>>>>>>>>>>>>", this.lastIndDetails);
+
+    console.log("vehicleEvent.siteId", vehicleEvent.site_type_id)
+    if (vehicleEvent.site_type_id) {
+      console.log("vehicleEvent.siteId", vehicleEvent.site_type_id)
+      this.fnLoadGeofence(vehicleEvent.site_type_id);
+    }
+  }
+
+
+  fnLoadGeofence(siteId) {
+    this.common.loading++;
+
+    let params = {
+      siteId: siteId
+    };
+
+    this.api.post('SiteFencing/getSiteFences', params)
+      .subscribe(res => {
+        let data = res['data'];
+        let count = Object.keys(data).length;
+        console.log('Res: ', res['data']);
+        if (count > 0) {
+          let latLngsArray = [];
+          let mainLatLng = null;
+          for (const datax in data) {
+            if (data.hasOwnProperty(datax)) {
+              const datav = data[datax];
+              if (datax == siteId)
+                mainLatLng = datav.latLngs;
+              latLngsArray.push(datav.latLngs);
+              console.log("Multi", datax);
+            }
+          }
+          this.createPolygons(latLngsArray, mainLatLng);
+        }
+        else {
+          console.log("Else");
+        }
+        this.common.loading--;
+      }, err => {
+        this.common.loading--;
+        this.common.showError(err);
+        console.log(err);
+      });
+  }
+
+  createPolygons(latLngsMulti, mainLatLngs?, options?) {// strokeColor = '#', fillColor = '#') {
+    if (this.polygons.length > 0) {
+      this.polygons.forEach(polygon => {
+        polygon.setMap(null);
+      });
+      this.polygons = [];
+    }
+    latLngsMulti.forEach(latLngs => {
+      let colorBorder = '#228B22';
+      let colorFill = '#ADFF2F';
+      if (mainLatLngs != latLngs) {
+        colorBorder = '#550000';
+        colorFill = '#ff7f7f';
+      }
+      const defaultOptions = {
+        paths: latLngs,
+        strokeColor: colorBorder,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        clickable: false,
+        fillColor: colorFill,
+        fillOpacity: 0.35
+      };
+      this.polygons.push(new google.maps.Polygon(options || defaultOptions));
+    });
+    this.polygons.forEach(polygon => {
+      polygon.setMap(this.map);
+    });
+
+  }
+
 
 }
