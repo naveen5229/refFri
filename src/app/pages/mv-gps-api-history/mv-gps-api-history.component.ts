f276bebf8ffbd6c6c6d68cb04d6d083f43d7e069
Lalit Singh <lalit.singh@elogist.in>
2019-11-04 16:48:25 +0530

    create mv-gps-history

--- src/app/pages/mv-gps-api-history/mv-gps-api-history.component.ts
+++ src/app/pages/mv-gps-api-history/mv-gps-api-history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../services/common.service';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'mv-gps-api-history',
@@ -6,10 +8,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mv-gps-api-history.component.scss']
 })
 export class MvGpsApiHistoryComponent implements OnInit {
-
-  constructor() { }
+  table = {
+    data: {
+      headings: {},
+      columns: []
+    },
+    settings: {
+      hideHeader: true
+    }
+  };
+  gpsApiHistory = [];
+  constructor(public common: CommonService,
+    public api: ApiService) {
+    this.getGpsApiHistory();
+    this.common.refresh = this.refresh.bind(this);
+  }
 
   ngOnInit() {
   }
+  refresh() {
+    this.getGpsApiHistory();
+  }
+
+  getGpsApiHistory() {
+    this.common.loading++;
+    this.api.get('GpsData/getGpsApiHistory')
+      .subscribe(res => {
+        console.log('Res:', res);
+        this.common.loading--;
+        this.resetTable();
+        if (!res['data']) {
+          this.common.showError("Data Not Found");
+          return;
+        }
+        this.gpsApiHistory = res['data'] || [];
+        this.setTable();
+      },
+        err => {
+          this.common.loading--;
+          this.common.showError(err);
+        });
+
+  }
+
+
+  setTable() {
+    this.table.data = {
+      headings: this.generateHeadings(this.gpsApiHistory[0]),
+      columns: this.getColumns(this.gpsApiHistory, this.gpsApiHistory[0])
+    };
+  }
+
+  generateHeadings(keyObject) {
+    let headings = {};
+    for (var key in keyObject) {
+      if (key.charAt(0) != "_") {
+        headings[key] = { title: this.formatTitle(key), placeholder: this.formatTitle(key) };
+      }
+    }
+    return headings;
+  }
+
+
+  formatTitle(title) {
+    return title.charAt(0).toUpperCase() + title.slice(1)
+  }
+
+  getColumns(historyList, headings) {
+    let columns = [];
+    historyList.map(item => {
+      let column = {};
+      for (let key in this.generateHeadings(headings)) {
+        if (key == "Action") {
+          column[key] = { value: "", action: null }
+        } else {
+          column[key] = { value: item[key], class: 'black', action: '' };
+        }
+      }
+      columns.push(column);
+    });
+    return columns;
+  }
+
+
+  resetTable() {
+    this.table = {
+      data: {
+        headings: {},
+        columns: []
+      },
+      settings: {
+        hideHeader: true
+      }
+    };
+  }
+
 
 }
