45f002158b1a16b7c2d4836e47c0001143850f16
Lalit Singh <lalit.singh@elogist.in>
2019-11-04 16:24:14 +0530

    create new page mv-gps-api history

---
+++ src/app/pages/mv-gps-api-history/mv-gps-api-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mv-gps-api-history',
+  templateUrl: './mv-gps-api-history.component.html',
+  styleUrls: ['./mv-gps-api-history.component.scss']
+})
+export class MvGpsApiHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
