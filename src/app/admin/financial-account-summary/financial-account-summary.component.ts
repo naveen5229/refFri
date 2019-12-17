b9ee773eac5a5f9064a757e12df80c6d44659cd4
Hemant <hemant1.axestrack@gmail.com>
2019-12-17 12:50:35 +0530

    committed financial account summary pdf

--- src/app/admin/financial-account-summary/financial-account-summary.component.ts
+++ src/app/admin/financial-account-summary/financial-account-summary.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../../services/api.service';
 import { UserService } from '../../services/user.service';
 import { DatePickerComponent } from '../../modals/date-picker/date-picker.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PdfService } from '../../services/pdf/pdf.service';
 
 @Component({
   selector: 'financial-account-summary',
@@ -12,17 +13,18 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class FinancialAccountSummaryComponent implements OnInit {
   dates = {
-    currentdate:this.common.dateFormatter(new Date()),
+    currentdate:this.common.dateFormatter1(new Date()),
+    
     start: null,
 
-    end: this.common.dateFormatter(new Date()),
+    end: this.common.dateFormatter1(new Date()),
   };
   fo = {
     id : null,
     name: null,
     mobileNo:null
   }
- 
+  result =[];
   data = [];
   openingBalance = 0;
   closingBalance = 0;
@@ -32,6 +34,7 @@ export class FinancialAccountSummaryComponent implements OnInit {
   mobileno = this.user._details.mobileno;
   foAgents=[];
   constructor(public api: ApiService,
+    public pdfService: PdfService,
     public common: CommonService,
     public user: UserService,
     public modalService: NgbModal, ) {
@@ -48,19 +51,24 @@ export class FinancialAccountSummaryComponent implements OnInit {
     //this.getaddTimeFinancialTollReport();
   }
 
-  calculateAmount(data)
+  calculateAmount(arr)
   {
-    for(let i=0;i<data.length;i++){
-			let usageStatus = data[i]['entry_type'];
-			let usageAmount = data[i]['amount'];
-			if(usageStatus.toLowerCase()!="balance recharge")
-			{
-				this.debitAmount += usageAmount;
-			}
-			else{
-				this.creditAmount += usageAmount;
-			}
-		}
+    let usageStatus = arr[0]['entry_type'];
+    let opening_balance_new = arr[0]['balance'];
+    let usageAmount = arr[0]['amount'];
+   
+       if( (usageStatus).toLowerCase!="usage")
+       {
+         //add to main amount
+         this.openingBalance = parseInt(opening_balance_new) + usageAmount;
+       }
+       else{
+         //subtract from main amount
+         this.openingBalance = parseInt(opening_balance_new) - (usageAmount);
+       }
+       this.closingBalance = arr[(arr.length-1)]['balance'];
+      //  console.log("opening_balance",opening_balance);
+      //  console.log("closing_balance",closing_balance);
   }
   getDate(date) {
     this.common.params = { ref_page: "card usage" };
@@ -75,13 +83,13 @@ export class FinancialAccountSummaryComponent implements OnInit {
   
   getaddTimeFinancialTollReport() {
     let params = "&startDate=" + this.dates.start + "&endDate=" + this.dates.end+"&mobileno="+this.fo.mobileNo;
-    // console.log("api hit");
     this.common.loading++;
     this.api.walle8Get('FinancialAccountSummary/getFinancialAccountSummaryAddTime.json?' + params)
       .subscribe(res => {
         this.common.loading--;
         console.log('Res:', res);
       if(res && res['data']){
+        this.result = res['data'];
         this.data = res['data'];
         this.openingBalance = this.data[0]['amount'];
         this.closingBalance = this.data[this.data.length-1]['amount'];
@@ -106,7 +114,7 @@ export class FinancialAccountSummaryComponent implements OnInit {
 
   getFoAgents(){
   let  search= document.getElementById('agentFo')['value'];
- console.log("searvh ",search)
+  console.log("searvh ",search)
     this.api.walle8Get('Suggestion/getFoAgents.json?search='+search)
       .subscribe(res => {
         console.log("res",res);
@@ -116,6 +124,15 @@ export class FinancialAccountSummaryComponent implements OnInit {
         console.log(err);
       });
   }
-
+  typedKey='';
+  filterData(event){
+    this.data = this.result.filter((ele)=>
+    {
+      if(!this.typedKey) 
+        return true; 
+      else 
+        return ele.vehid?ele.vehid.toLowerCase().includes(this.typedKey):false;
+    })  
+  }
   
 }
