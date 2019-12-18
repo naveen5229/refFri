9b838aedfd75c979aa74dc4cda48e9dff08a4d90
Lalit Singh <singhlalit843@gmail.com>
2019-12-18 17:35:10 +0530

    changes in page menu

--- src/app/walle8/walle8-menu.ts
+++ src/app/walle8/walle8-menu.ts
@@ -4,7 +4,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
     {
         title: 'Home Dashboard',
         icon: 'fa fa-home',
-        link: '/walle8',
+        link: '/pages/dashboard',
         home: true,
     },
 
