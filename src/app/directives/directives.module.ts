2b77d41071cfba97a27d45a502dc7c958b604bae
The Jai Rana <jkrana008@gmail.com>
2020-06-11 17:03:05 +0530

    Accounts More Data Issue

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -8,6 +8,7 @@ import { SmartTableComponent } from './smart-table/smart-table.component';
 import { AutoSuggetionInSideComponent } from './auto-suggetion-in-side/auto-suggetion-in-side.component';
 import { DateTimePickerComponent } from './date-time-picker/date-time-picker.component';
 import { MonthPickerComponent } from './month-picker/month-picker.component';
+import { LazyForDirective } from './lazyFor.directive';
 
 
 @NgModule({
@@ -20,10 +21,12 @@ import { MonthPickerComponent } from './month-picker/month-picker.component';
         AutoSuggetionInSideComponent,
         DateTimePickerComponent,
         MonthPickerComponent,
+        LazyForDirective
     ],
     declarations: [AutoSuggestionComponent, SmartTableComponent, AutoSuggetionInSideComponent,
         DateTimePickerComponent,
         MonthPickerComponent,
+        LazyForDirective
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     providers: [],
