4f5a46fa0a5f86104bc6e45e97b589d28ea84b86
The Jai Rana <jkrana008@gmail.com>
2020-06-27 12:11:50 +0530

    Qr Code Remove Cursor & Dashboard Concise View Origin Status Color Code

--- src/app/pages/concise/concise.component.ts
+++ src/app/pages/concise/concise.component.ts
@@ -564,10 +564,10 @@ export class ConciseComponent implements OnInit {
           action: this.handleMapView.bind(this),
 
         },
-        pagination :true,
+        pagination: true,
         tableHeight: "87vh"
       },
-      
+
 
     };
   }
@@ -742,9 +742,9 @@ export class ConciseComponent implements OnInit {
 
   createCluster() {
     if (this.isCluster) {
-      for(let i=0;i<this.markers.length;i++){
-        if(this.markers[i])
-        this.markers[i].title = this.kpis[i].x_showveh;
+      for (let i = 0; i < this.markers.length; i++) {
+        if (this.markers[i])
+          this.markers[i].title = this.kpis[i].x_showveh;
       }
       this.mapService.createCluster(this.markers, true);
     } else {
@@ -895,17 +895,17 @@ export class ConciseComponent implements OnInit {
       latch_time: latch_time,
       status: 2,
       remark: trip.remark,
-      regno : trip.x_showveh,
-      tripName : this.common.getTripStatusHTML(trip.trip_status_type, trip.x_showtripstart, trip.x_showtripend, trip.x_p_placement_type, trip.x_p_loc_name)
+      regno: trip.x_showveh,
+      tripName: this.common.getTripStatusHTML(trip.trip_status_type, trip.x_showtripstart, trip.x_showtripend, trip.x_p_placement_type, trip.x_p_loc_name)
     };
     this.common.ref_page = 'tsfl';
     this.common.params = VehicleStatusData;
     if (this.user._loggedInBy != "admin") {
       this.modalService.open(ChangeVehicleStatusByCustomerComponent, { size: 'lg', container: 'nb-layout' });
     }
-    else{
-    this.modalService.open(ChangeVehicleStatusComponent, { size: 'lg', container: 'nb-layout' });
-  }
+    else {
+      this.modalService.open(ChangeVehicleStatusComponent, { size: 'lg', container: 'nb-layout' });
+    }
   }
 
 
@@ -974,10 +974,10 @@ export class ConciseComponent implements OnInit {
 
   }
 
-  openVehicleWiseOrders(data){
+  openVehicleWiseOrders(data) {
     let vehicle = {
-      id : data.x_vehicle_id,
-      regno : data.x_showveh
+      id: data.x_vehicle_id,
+      regno: data.x_showveh
 
     }
     this.common.params = { vehicle: vehicle };
