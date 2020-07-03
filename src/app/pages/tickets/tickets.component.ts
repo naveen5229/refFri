bf45723292ce3a52ad21eb625f03afbac7426338
Hemant <hemant1.axestrack@gmail.com>
2020-07-03 10:18:14 +0530

    changes committed regarding showtrails

--- src/app/pages/tickets/tickets.component.ts
+++ src/app/pages/tickets/tickets.component.ts
@@ -191,8 +191,9 @@ export class TicketsComponent implements OnInit {
         let trailList = res['data'];
         if(trailList){
         console.log("DataTrail:",res['data']);
+        let type="trail"
         let headers = ["#", "Employee Name", "Spent Time", "Status"];
-        this.common.params = { trailList, headers };
+        this.common.params = { trailList, headers,type };
         const activeModal = this.modalService.open(TicketTrailsComponent, { size: 'lg', container: 'nb-layout' });
         activeModal.componentInstance.modalHeader = 'Trails';
         }else{
