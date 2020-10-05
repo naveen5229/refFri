2a387cd0d57a2444c2a85e290159d54668c14fbe
prashant <prashantmaiet2012@gmail.com>
2020-10-05 12:34:38 +0530

    emp dsash

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1995,4 +1995,22 @@ export class CommonService {
     return chartObj;
   }
 
+  imageDownloadFromUrl(url, fileName){
+    console.log("url, fileName",url, fileName);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function(){
+        var urlCreator = window.URL;
+        var imageUrl = urlCreator.createObjectURL(this.response);
+        var tag = document.createElement('a');
+        tag.href = imageUrl;
+        tag.download = fileName;
+        document.body.appendChild(tag);
+        tag.click();
+        document.body.removeChild(tag);
+    }
+    xhr.send();
+}
+
 }
