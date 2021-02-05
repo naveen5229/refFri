25cca3cd607c2fa8e473bc6acce481a32b4b1c07
deedurgesh <durgesh.gupta@axestrack.com>
2021-02-05 12:58:25 -0800

    Data is getting from verify filters

--- src/app/modals/verify-halts/verify-halts.component.ts
+++ src/app/modals/verify-halts/verify-halts.component.ts
@@ -2,71 +2,63 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../../services/api.service";
 import { CommonService } from "../../services/common.service";
-import verifyHalts from '../../../assets/verify-halts.json';
 import { FormGroup } from "@angular/forms";
 
 
 @Component({
-    selector: 'verify-halts',
-    templateUrl: './verify-halts.component.html',
-    styleUrls: ['./verify-halts.component.scss']
-  })
-  export class VerifyHaltsComponent implements OnInit {
+  selector: 'verify-halts',
+  templateUrl: './verify-halts.component.html',
+  styleUrls: ['./verify-halts.component.scss']
+})
+export class VerifyHaltsComponent implements OnInit {
 
-    verifyHaltsForm: FormGroup;
+  verifyHaltsForm: FormGroup;
 
-    submitted = false;
-    filterVar = {
-      vehicleId: '',
-      startTime: new Date(),
-      endTime: new Date(),
-    }
-    haltExistingData:[] = verifyHalts;
-    haltNew = []
-    haltExisting = []
-    
-
-    constructor(private common: CommonService, private api: ApiService, private httpCLient: HttpClient){
-    }
-
-    ngOnInit(){}
-
-    getVerifyHaltsFilterData(){
-      let params = {
-        vehicleId: this.filterVar.vehicleId,
-        startTime: this.common.dateFormatter(this.filterVar.startTime),
-        endTime: this.common.dateFormatter(this.filterVar.endTime).split(' ')[0],
-      }
-      console.log("params", params);
-    //   this.common.loading++;
-    //   this.api.verifyHaltsGet('autoHaltsGeneration?', this.getParamsData())
-    //   .subscribe(res=> {
-    //    this.common.loading--;
-    //    console.log("response data", JSON.stringify(res));
-    //    let data = [];
-    //     console.log('res', res['data']);
-
-    //   })
-    //     console.log('get verify-halts data');     
-    //    }
-    
-    // getParamsData(){
-    //    return `vehId=${this.filterVar.vehicleId}&startTime=${2020-12-20%2000:00:00}&endTime=${2020-12-25%2023:59:59} `
-    // }
-    
-
-    // getVerifyHaltsFilterData(){
-    //   console.log(JSON.stringify(this.haltExistingData));
-     
-    //   this.haltExisting = this.haltExistingData['haltExisting']
-    //   this.haltNew = this.haltExistingData['haltNew'];
-
-    }
-
-    getVehicleData(vehicle){
-      console.log("vehicle", vehicle);
-      this.filterVar.vehicleId = vehicle.id;
-    }
-
-
+  submitted = false;
+  filterVar = {
+    vehicleId: '',
+    startTime: new Date(),
+    endTime: new Date(),
   }
+  haltNew = []
+  haltExisting = []
+
+  constructor(private common: CommonService, private api: ApiService, private httpCLient: HttpClient) {
+    this.filterVar.startTime = new Date();
+    this.filterVar.endTime = new Date();
+  }
+
+  ngOnInit() { }
+
+  getVerifyHaltsFilterData() {
+    let params = {
+      vehicleId: this.filterVar.vehicleId,
+      startTime: this.common.dateFormatter(this.filterVar.startTime),
+      endTime: this.common.dateFormatter(this.filterVar.endTime),
+    }
+    console.log("params", params);
+    this.common.loading++;
+    this.api.verifyHaltsGet('autoHaltsGeneration?', `vehId=${params.vehicleId}&startTime=${params.startTime}&endTime=${params.endTime}`)
+      .subscribe(res => {
+        this.common.loading--;
+        this.haltExisting = res['haltExisting'];
+        this.haltNew = res['haltNew'];
+        console.log('halfExisting data :', this.haltExisting);
+        console.log('haltNew data :', this.haltNew);
+      },
+        err => {
+          console.error(err);
+          this.common.showError();
+        });
+  }
+
+  getParamsData() {
+    return `vehId=${this.filterVar.vehicleId}&startTime=${this.filterVar.startTime}&endTime=2020-12-25 2023:59:59`
+  }
+
+  getVehicleData(vehicle) {
+    console.log("vehicle", vehicle);
+    this.filterVar.vehicleId = vehicle.id;
+  }
+
+}
