f1a369b1d4546928b6db2dbb3bf293cbd004191a
Hemant Sisodia <hemant1.axestrack@gmail.com>
2021-02-23 17:23:54 +0530

    changes committed on user call summary

--- src/app/walle8/financial-toll-summary-addtime/financial-toll-summary-addtime.component.ts
+++ src/app/walle8/financial-toll-summary-addtime/financial-toll-summary-addtime.component.ts
@@ -21,19 +21,12 @@ import { AutoUnsubscribe } from "ngx-auto-unsubscribe";
 })
 export class FinancialTollSummaryAddtimeComponent implements OnInit {
 
-  // page = 1;
-  // pageSize = 200;
   dates = {
     currentdate: this.common.dateFormatter1(new Date())
-    // start: null,
-    // end: this.common.dateFormatter1(new Date()),
   };
-
   startDate = (new Date(new Date().setDate(new Date().getDate() - 7)));
   minDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
   endDate = new Date();
-
-
 
   fo = {
     id: null,
@@ -64,28 +57,17 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
     private pdfService: PdfService,
     private excelService: ExcelService,
     private csvService: CsvService) {
-    // this.foid = this.user._details.foid;
-
     console.log("this.user._details.", this.user._details);
     this.fo.id = this.user._loggedInBy == 'admin' ? this.user._customer.foid : this.user._details.foid;
     this.fo.mobileNo = this.user._details.fo_mobileno;
     this.fo.name = this.user._loggedInBy == 'admin' ? this.user._details.username : this.user._details.foName;
     console.log("FoName:", this.user._details.username);
     this.common.refresh = this.refresh.bind(this);
-
   }
 
   ngOnDestroy() { }
   ngOnInit() {
   }
-
-
-  // customPage(event){
-  //   console.log('Event:', event);
-  //   event.preventDefault();
-  //   event.stopPropagation();
-  //   this.cdr.detectChanges();
-  // }
 
   selectVehicle(vehData) {
     this.vehId = vehData.id;
@@ -101,7 +83,6 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
     console.log(this.data);
   }
 
-
   refresh() {
     this.getaddTimeFinancialTollReport();
   }
@@ -110,10 +91,8 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
     let usageStatus = arr[0]['entry_type'];
     let opening_balance_new = arr[0]['balance'];
     let usageAmount = arr[0]['amount'];
-
     console.log("usageStatus", (usageStatus).toLowerCase, "opening_balance_new", opening_balance_new, "usageAmount", usageAmount)
     if ((usageStatus).toLowerCase() == "usage" || (usageStatus).toLowerCase() == "balance recharge") {
-
       this.openingBalance = parseInt(opening_balance_new) - usageAmount;
     } else {
       this.openingBalance = parseInt(opening_balance_new) + (usageAmount);
@@ -122,7 +101,6 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
   }
 
   getaddTimeFinancialTollReport() {
-    // this.page = 0;
     this.openingBalance = 0;
     this.closingBalance = 0;
     this.data = [];
@@ -147,9 +125,7 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
           console.log(err);
         });
     }
-
   }
-
 
   setTable() {
     let headings = {
@@ -227,128 +203,7 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
         return ele.vehid ? ele.vehid.toLowerCase().includes(this.typedKey) : false;
       }
     })
-    console.log("data", this.data);
   }
-
-  // generatePDF() {
-  //   let doc = new jsPDF({
-  //     orientation: 'l',
-  //     unit: "px",
-  //     format: "a4"
-  //   });
-
-  //   doc = this.setPdfHeader(doc);
-  //   console.log("----------------",doc);
-
-  //   let startingPage = doc.internal.getCurrentPageInfo().pageNumber;
-  //   doc.autoTable(this.getPdfTableConfig('provider', { right: 330, left: 30, top: 20 }, null, 70, 140));
-  //   doc.setPage(startingPage);
-  //   doc.autoTable(this.getPdfTableConfig('customer', { left: 330, right: 30, top: 20 }, null, 70, 140));
-  //   doc.autoTable(this.getPdfTableConfig('account', { left: 30, right: 30, top: 0 }, null, null, 290));
-  //   doc.autoTable(this.getPdfTableConfig('tblData', { left: 30, right: 30, top: 0 }, this.setPdfPageFooter.bind(this, doc)));
-  //   doc.save('financial-toll-summary.pdf');
-  // }
-
-  // setPdfHeader(doc) {
-  //   doc.setFontSize(12);
-  //   doc.setFontStyle('bold');
-  //   doc.text(30, 30, 'eLogist Solutions Pvt. Ltd.');
-
-  //   doc.setFontSize(8)
-  //   doc.setFontStyle('normal');
-  //   doc.text(30, 39, 'Address: 605-21, Jaipur Electronic Market,');
-  //   doc.text(30, 46, 'Riddhi Siddhi Circle, Gopalpura Bypass, Jaipur, Rajasthan - 302018');
-  //   doc.text(30, 53, 'Support: 8081604455');
-  //   doc.text(30, 60, 'Website: www.walle8.com');
-
-  //   // let image = this.getImgTagToBase64('logo');
-  //   // doc.addImage(image, 'PNG', 550, 15, 40, 40);
-
-  //   return doc;
-  // }
-
-  // setPdfPageFooter(doc, data) {
-  //   doc.setFont("times", "bold");
-  //   doc.setFontSize(10);
-  //   doc.text(
-  //     'Page ' + data.pageCount,
-  //     data.settings.margin.left,
-  //     doc.internal.pageSize.height - 10
-  //   );
-  //   /******** Page Top Margin *********** */
-  //   data.settings.margin.top = 20;
-  // };
-
-  // getPdfTableConfig(id: string, margin: object, footerSetter?: any, startY?: number, cellWidth = 50) {
-  //   let config = {
-  //     html: '#' + id,
-  //     theme: 'grid',
-  //     margin: margin,
-  //     rowPageBreak: 'avoid',
-  //     headStyles: {
-  //       fillColor: [98, 98, 98],
-  //       fontSize: 10,
-  //       halign: 'center',
-  //       valign: 'middle'
-  //     },
-  //     styles: {
-  //       fontSize: 10,
-  //       cellPadding: 3,
-  //       minCellHeight: 11,
-  //       minCellWidth: 10,
-  //       cellWidth: cellWidth,
-  //       valign: 'middle',
-  //       halign: 'center'
-  //     },
-  //     columnStyles: {
-  //       text: {
-  //         cellWidth: cellWidth,
-  //         halign: 'center',
-  //         valign: 'middle'
-  //       }
-  //     },
-  //   }
-
-  //   if (startY)
-  //     config['startY'] = startY;
-
-  //   if (footerSetter)
-  //     config['didDrawPage'] = footerSetter;
-
-  //   return config;
-  // }
-
-  // getImgTagToBase64(id: string) {
-  //   let canvas = document.createElement('canvas');
-  //   let img: any = document.getElementById(id);
-  //   canvas.height = img.naturalHeight;
-  //   canvas.width = img.naturalWidth;
-  //   canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
-  //   return canvas.toDataURL();
-  // }
-
-
-  // printCsv(tblEltId, tblEltId1) {
-  //   let customerName = this.user._customer.name;
-  //   if (this.user._loggedInBy == "customer")
-  //     customerName = this.user._details.name;
-  //   let details = [
-  //     { customer: 'Customer : ' + this.fo.name },
-  //     { report: 'Report : Financial Toll Summary (Add Time)' },
-  //     { period: 'Period : ' + this.common.dateFormatter1(this.startDate) + " To " + this.common.dateFormatter1(this.endDate) },
-  //     { time: 'Time : ' + this.datePipe.transform(new Date(), 'dd-MM-yyyy hh:mm:ss a') }
-  //   ];
-  //   this.csvService.byMultiIds([tblEltId1], 'Financial Toll Summary', details);
-  // }
-
-  // printCsv(){
-  //   let name=this.user._loggedInBy=='admin' ? this.user._details.username : this.user._details.foName;
-  //   let details = [
-  //     { name: 'Name:' + name,startdate:'Start Date:'+this.common.dateFormatter1(this.startDate),enddate:'End Date:'+this.common.dateFormatter1(this.endDate), report:"Report:Financial Toll Summary Add Time"}
-  //   ];
-  //   this.csvService.byMultiIds(['tblData'], 'Financial Toll Summary Add Time', details);
-  // }
-
 
   printCsv() {
     let startDate = this.common.dateFormatter1(this.startDate);
@@ -374,7 +229,6 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
         "Transaction ID": fts['txn_id'],
       };
     });
-
     this.excelService.jrxExcel("Financial Toll Summary Add Time", headerDetails, headersArray, json, 'Financial Toll Summary Add Time', false);
   }
 
@@ -392,7 +246,6 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
         ['Name: ' + name, 'Start Date: ' + this.common.dateFormatter1(this.startDate), 'End Date: ' + this.common.dateFormatter1(this.endDate), 'Report: ' + 'Financial Toll Summary Add Time']
       ];
       this.pdfService.jrxTablesPDF(['provider', 'customer', 'account', 'tblData'], 'Financial Toll Summary Add Time', details);
-     
       this.table.settings = {
         pagination: true,
         hideHeader: true,
@@ -402,5 +255,4 @@ export class FinancialTollSummaryAddtimeComponent implements OnInit {
     }, 2000);
     this.common.loading--;
   }
-
 }
