78e7d5d315ae539276dc708dedd94118f65750fc
deedurgesh <durgesh.gupta@axestrack.com>
2021-05-21 14:37:13 +0530

    trip-master-report smart-table created with hyperlink

--- src/app/pages/tripmasterreport/tripmasterreport.component.ts
+++ src/app/pages/tripmasterreport/tripmasterreport.component.ts
@@ -14,7 +14,9 @@ import { RouteMapper } from '../../modals';
   templateUrl: './tripmasterreport.component.html',
   styleUrls: ['./tripmasterreport.component.scss']
 })
+
 export class TripmasterreportComponent implements OnInit {
+
   tripData = [];
   tripMasterReportData = [];
   selectedVehicle = {
@@ -52,49 +54,118 @@ export class TripmasterreportComponent implements OnInit {
     console.log('test fase', this.selectedVehicle.id);
   }
 
-  // getTripMasterReport(){
-
-  // }
-
   getTripMasterReport() {
-    this.tripData = [];
     const params = "vid=" + this.selectedVehicle.id + "&startTime=" + this.common.dateFormatter(this.startTime) +
       "&endTime=" + this.common.dateFormatter(this.endTime);
     this.common.loading++;
-    this.api.get('TripsOperation/getTripMasterReport?' + params)
+    this.api.get('TripsOperation/getTripMasterReportv1?' + params)
       .subscribe(res => {
         this.common.loading--;
-        if (res['code'] > 0) {
-          this.tripMasterReportData = res['data'] ? res['data'] : [];
-          this.tripData = this.tripMasterReportData.map(item => {
-            item.origin = item['trp_points'][0];
-            let last = item['trp_points'].length - 1;
-            item.via = item['trp_points'].length > 2 ? item['trp_points'][1] : '';
-            item.destination = item['trp_points'].length >= 2 ? item['trp_points'][last] : '';
+        this.tripData = res['data'];
+        let tripDataKey = this.tripData[0];
+        console.log('response is: ', this.tripData);
 
-            console.log("origin:", item.origin);
-            console.log("via:", item.via);
-            console.log("destination:", item.destination);
-
-            // item['trp_points'][1]='Via';
-            // item['trp_points'][last]='Destination';
-            return item;
-          })
-
-          this.createSmartTableData();
-
-          console.log("tripData:", this.tripData);
-
-
-          console.log("data:", this.tripMasterReportData);
-        } else {
-          this.common.showError(res['msg']);
+        let headings = {};
+        for (var key in tripDataKey) {
+          if (key.charAt(0) != "_") {
+            this.headings.push(key);
+            let headerObj = { title: this.formatTitle(key), placeholder: this.formatTitle(key) };
+            headings[key] = headerObj;
+          }
         }
+        this.table.data = {
+          headings: headings,
+          columns: this.getTableColumns()
+        };
       }, err => {
         this.common.loading--;
         this.common.showError();
       })
   }
+
+
+  getTableColumns() {
+    let columns = [];
+    this.tripData.map(matrix => {
+      this.valobj = {};
+      for (let i = 0; i < this.headings.length; i++) {
+        if(this.headings[i] === 'AI Repaired Distance'){
+          this.valobj[this.headings[i]] = { value: matrix[this.headings[i]], class: 'blue', action: this.aiRepairedDistanceAction.bind(this, matrix) };
+        } else if(this.headings[i] === 'Toll Count'){
+          this.valobj[this.headings[i]] = { value: matrix[this.headings[i]], class: 'blue', action: this.tollCountAction.bind(this, matrix) };
+        }  else if(this.headings[i] === 'Halt'){
+          this.valobj[this.headings[i]] = { value: matrix[this.headings[i]], class: 'blue', action: this.aiRepairedDistanceAction.bind(this, matrix) };
+        } else this.valobj[this.headings[i]] = { value: matrix[this.headings[i]], class: 'black', action: '' };
+      }
+      columns.push(this.valobj);
+    });
+    return columns;
+  }
+
+  aiRepairedDistanceAction(data){
+    console.log("aiRepairedDistanceAction data :" , data);
+  }
+
+  tollCountAction(data){
+    console.log("aiRepairedDistanceAction data :" , data);
+  }
+
+  haltAction(data){
+    console.log("aiRepairedDistanceAction data :" , data);
+  }
+
+  formatTitle(strval) {
+    let pos = strval.indexOf('_');
+    if (pos > 0) {
+      return strval.toLowerCase().split('_').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+    } else {
+      return strval.charAt(0).toUpperCase() + strval.substr(1);
+    }
+  }
+
+
+
+
+
+  // getTripMasterReport() {
+  //   this.tripData = [];
+  //   const params = "vid=" + this.selectedVehicle.id + "&startTime=" + this.common.dateFormatter(this.startTime) +
+  //     "&endTime=" + this.common.dateFormatter(this.endTime);
+  //   this.common.loading++;
+  //   this.api.get('TripsOperation/getTripMasterReport?' + params)
+  //     .subscribe(res => {
+  //       this.common.loading--;
+  //       if (res['code'] > 0) {
+  //         this.tripMasterReportData = res['data'] ? res['data'] : [];
+  //         this.tripData = this.tripMasterReportData.map(item => {
+  //           item.origin = item['trp_points'][0];
+  //           let last = item['trp_points'].length - 1;
+  //           item.via = item['trp_points'].length > 2 ? item['trp_points'][1] : '';
+  //           item.destination = item['trp_points'].length >= 2 ? item['trp_points'][last] : '';
+
+  //           console.log("origin:", item.origin);
+  //           console.log("via:", item.via);
+  //           console.log("destination:", item.destination);
+
+  //           // item['trp_points'][1]='Via';
+  //           // item['trp_points'][last]='Destination';
+  //           return item;
+  //         })
+
+  //         this.createSmartTableData();
+
+  //         console.log("tripData:", this.tripData);
+
+
+  //         console.log("data:", this.tripMasterReportData);
+  //       } else {
+  //         this.common.showError(res['msg']);
+  //       }
+  //     }, err => {
+  //       this.common.loading--;
+  //       this.common.showError();
+  //     })
+  // }
 
   openTripKmRepair(data) {
     console.log("KMdata:", data);
@@ -144,93 +215,93 @@ export class TripmasterreportComponent implements OnInit {
     });
   }
 
-  createSmartTableData() {
-    let headings = {
-      trip_id: { placeholder: 'Trip Id' },
-      regno: { placeholder: 'Vehicle No.' },
-      trip_onward_status: { placeholder: 'Onward Status' },
-      inv_start_time: { placeholder: 'Invoice Date & Time' },
-      receipt_no: { placeholder: 'LR No.' },
-      product_name: { placeholder: 'Product Name' },
-      route_name: { placeholder: 'Route Name' },
-      lead_dis: { placeholder: 'Lead Distance' },
-      gps_dis: { placeholder: 'GPS Distance' },
-      route_kms: { placeholder: 'AI Repaired Distance' },
-      lead_dis_deviation: { placeholder: 'Lead Distance Deviation' },
-      dest_deviation: { placeholder: 'Destination Deviation' },
-      cnee: { placeholder: 'Consignee' },
-      cnor: { placeholder: 'Consignor' },
-      mode: { placeholder: 'Mode' },
-      gps_per: { placeholder: 'GPS %' },
-      sim_per: { placeholder: 'SIM %' },
-      org_loc_name: { placeholder: 'Org Location' },
-      org_trans_time: { placeholder: 'Org Reporting Time' },
-      org_act_start_time: { placeholder: 'Org Entry Time' },
-      org_act_end_time: { placeholder: 'Org Exit Time' },
-      org_load_time: { placeholder: 'Org Detention' },
-      org_delay: { placeholder: 'Org Delay' },
-      via_loc_name: { placeholder: 'Via Location' },
-      via_trans_time: { placeholder: 'Via Reporting Time' },
-      via_act_start_time: { placeholder: 'Via Entry Time' },
-      via_act_end_time: { placeholder: 'Via Exit Time' },
-      via_dest_name: { placeholder: 'Via Detention' },
-      via_delay: { placeholder: 'Via Delay' },
-      dest_loc_name: { placeholder: 'Dest Location' },
-      dest_trans_time: { placeholder: 'Dest Reporting Time' },
-      dest_act_start_time: { placeholder: 'Dest Entry Time' },
-      dest_act_end_time: { placeholder: 'Dest Exit Time' },
-      unload_time: { placeholder: 'Dest Detention' },
-      dest_delay: { placeholder: 'Dest Delay' },
-      trip_time: { placeholder: 'Total Trip Time' },
-      trip_delay: { placeholder: 'Total Trip Delay' },
-      weight: { placeholder: 'Weight' },
-      toll_count: { placeholder: 'Toll Count' },
-      stoppagecount: { placeholder: 'Halt' },
-      driver_name: { placeholder: 'Driver Name' },
-      driver_mobile: { placeholder: 'Driver Mobile' },
-      expire_veh_count: { placeholder: 'Expired Doc. Count' },
-      challan_amount: { placeholder: 'Challan date/Amount' },
-      trip_stamp_status: { placeholder: 'Stamp Status' }
-    }
-    let rows = this.tripData.map(trip => {
-      let row = {};
-      Object.keys(headings)
-        .map(heading => {
-          let value = trip[heading];
-          if (heading.startsWith('org_')) {
-            value = trip.origin[heading.replace('org_', '')];
-            if (heading.endsWith('load_time')) {
-              value = trip.load_time;
-            }
-          } else if (heading.startsWith('via_')) {
-            value = trip.via[heading.replace('org_', '')];
-          } else if (heading.startsWith('dest_')) {
-            value = trip.destination[heading.replace('dest_', '')];
-            if (heading.endsWith('unload_time')) {
-              value = trip.unload_time;
-            }
-          }
-          let action = null;
-          let className = null;
-          if (heading == 'route_kms') {
-            action = this.openTripKmRepair.bind(this, trip);
-            className = 'blue';
-          } else if (heading == 'toll_count') {
-            action = this.tollPaymentManagement.bind(this, trip);
-            className = 'blue';
-          } else if (heading == 'stoppagecount') {
-            action = this.showRouteMapper.bind(this, trip);
-            className = 'blue';
-          }
-          row[heading] = { value, action, class: className }
-        });
-      return row;
-    });
-    this.table.data = {
-      headings,
-      columns: rows
-    }
-  }
+  // createSmartTableData() {
+  //   let headings = {
+  //     trip_id: { placeholder: 'Trip Id' },
+  //     regno: { placeholder: 'Vehicle No.' },
+  //     trip_onward_status: { placeholder: 'Onward Status' },
+  //     inv_start_time: { placeholder: 'Invoice Date & Time' },
+  //     receipt_no: { placeholder: 'LR No.' },
+  //     product_name: { placeholder: 'Product Name' },
+  //     route_name: { placeholder: 'Route Name' },
+  //     lead_dis: { placeholder: 'Lead Distance' },
+  //     gps_dis: { placeholder: 'GPS Distance' },
+  //     route_kms: { placeholder: 'AI Repaired Distance' },
+  //     lead_dis_deviation: { placeholder: 'Lead Distance Deviation' },
+  //     dest_deviation: { placeholder: 'Destination Deviation' },
+  //     cnee: { placeholder: 'Consignee' },
+  //     cnor: { placeholder: 'Consignor' },
+  //     mode: { placeholder: 'Mode' },
+  //     gps_per: { placeholder: 'GPS %' },
+  //     sim_per: { placeholder: 'SIM %' },
+  //     org_loc_name: { placeholder: 'Org Location' },
+  //     org_trans_time: { placeholder: 'Org Reporting Time' },
+  //     org_act_start_time: { placeholder: 'Org Entry Time' },
+  //     org_act_end_time: { placeholder: 'Org Exit Time' },
+  //     org_load_time: { placeholder: 'Org Detention' },
+  //     org_delay: { placeholder: 'Org Delay' },
+  //     via_loc_name: { placeholder: 'Via Location' },
+  //     via_trans_time: { placeholder: 'Via Reporting Time' },
+  //     via_act_start_time: { placeholder: 'Via Entry Time' },
+  //     via_act_end_time: { placeholder: 'Via Exit Time' },
+  //     via_dest_name: { placeholder: 'Via Detention' },
+  //     via_delay: { placeholder: 'Via Delay' },
+  //     dest_loc_name: { placeholder: 'Dest Location' },
+  //     dest_trans_time: { placeholder: 'Dest Reporting Time' },
+  //     dest_act_start_time: { placeholder: 'Dest Entry Time' },
+  //     dest_act_end_time: { placeholder: 'Dest Exit Time' },
+  //     unload_time: { placeholder: 'Dest Detention' },
+  //     dest_delay: { placeholder: 'Dest Delay' },
+  //     trip_time: { placeholder: 'Total Trip Time' },
+  //     trip_delay: { placeholder: 'Total Trip Delay' },
+  //     weight: { placeholder: 'Weight' },
+  //     toll_count: { placeholder: 'Toll Count' },
+  //     stoppagecount: { placeholder: 'Halt' },
+  //     driver_name: { placeholder: 'Driver Name' },
+  //     driver_mobile: { placeholder: 'Driver Mobile' },
+  //     expire_veh_count: { placeholder: 'Expired Doc. Count' },
+  //     challan_amount: { placeholder: 'Challan date/Amount' },
+  //     trip_stamp_status: { placeholder: 'Stamp Status' }
+  //   }
+  //   let rows = this.tripData.map(trip => {
+  //     let row = {};
+  //     Object.keys(headings)
+  //       .map(heading => {
+  //         let value = trip[heading];
+  //         if (heading.startsWith('org_')) {
+  //           value = trip.origin[heading.replace('org_', '')];
+  //           if (heading.endsWith('load_time')) {
+  //             value = trip.load_time;
+  //           }
+  //         } else if (heading.startsWith('via_')) {
+  //           value = trip.via[heading.replace('org_', '')];
+  //         } else if (heading.startsWith('dest_')) {
+  //           value = trip.destination[heading.replace('dest_', '')];
+  //           if (heading.endsWith('unload_time')) {
+  //             value = trip.unload_time;
+  //           }
+  //         }
+  //         let action = null;
+  //         let className = null;
+  //         if (heading == 'route_kms') {
+  //           action = this.openTripKmRepair.bind(this, trip);
+  //           className = 'blue';
+  //         } else if (heading == 'toll_count') {
+  //           action = this.tollPaymentManagement.bind(this, trip);
+  //           className = 'blue';
+  //         } else if (heading == 'stoppagecount') {
+  //           action = this.showRouteMapper.bind(this, trip);
+  //           className = 'blue';
+  //         }
+  //         row[heading] = { value, action, class: className }
+  //       });
+  //     return row;
+  //   });
+  //   this.table.data = {
+  //     headings,
+  //     columns: rows
+  //   }
+  // }
 
   downloadPdf() {
     this.common.getPDFFromTableId('trip-master-report');
