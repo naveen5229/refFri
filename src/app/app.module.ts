d5eb7cf5bdcca0a7d51086eae42117104c0b1c84
Hemant Sisodia <hemant1.axestrack@gmail.com>
2021-05-25 17:04:58 +0530

    changes committed reg cost matrix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -319,6 +319,7 @@ import { NearByVehiclesComponent } from './modals/near-by-vehicles/near-by-vehic
 import { ConciseColumnPrefrenceComponent } from './modals/concise-column-prefrence/concise-column-prefrence.component';
 import {MatRadioModule} from '@angular/material/radio';
 import { AddGpsNewRequesComponent } from './modals/add-gps-new-reques/add-gps-new-reques.component';
+import { CostmatrixComponent } from './modals/costmatrix/costmatrix.component';
 
 
 
@@ -591,7 +592,8 @@ const COMMON_COMPONENT = [UnMergeStateComponent,
   DriverConsentListComponent,
   PlacementProblemGenerationComponent,
   ConciseColumnPrefrenceComponent,
-  AddGpsNewRequesComponent
+  AddGpsNewRequesComponent,
+  CostmatrixComponent
 ];
 
 @NgModule({
@@ -619,11 +621,7 @@ const COMMON_COMPONENT = [UnMergeStateComponent,
     PlacementProblemGenerationComponent,
     NearByVehiclesComponent,
     AddGpsNewRequesComponent,
-    
-   
-   
-    
-    
+    CostmatrixComponent,
   ],
   entryComponents: [
     ...COMMON_COMPONENT,
