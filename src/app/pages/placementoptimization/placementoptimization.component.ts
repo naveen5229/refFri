44669e55eb56b4f3100f4511dbc8ecd2080bcb51
deedurgesh <durgesh.gupta@axestrack.com>
2021-07-01 14:16:59 +0530

    floating button added in placement-optimization

--- src/app/pages/placementoptimization/placementoptimization.component.ts
+++ src/app/pages/placementoptimization/placementoptimization.component.ts
@@ -382,14 +382,24 @@ export class PlacementoptimizationComponent implements OnInit {
   }
 
   scrollToView(event){
+    
     console.log('event data is: ', event)
     var element = document.getElementById('plant-details');
     let siteId = event.target.value.siteId;
     let dayIndex = event.target.value.dayIndex;
     var ele2 = document.getElementById(siteId);
+    this.placementOPT.siteVehicleCostPackets.forEach(element => {
+      element.active = false;
+    });
     let activeObj = this.placementOPT.siteVehicleCostPackets.find(ele=> (ele.siteId == siteId && ele.dayIndex == dayIndex));
     activeObj.active = true;
     element.scrollIntoView();
     ele2.scrollIntoView();
+  }
+
+  // site-details
+  backToScrollView(){
+    var ele = document.getElementById('site-details');
+    ele.scrollIntoView()
   }
 }
